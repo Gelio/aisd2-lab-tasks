72e34da1661d401931d6f861641806fa587211f4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-02-16 11:26:36 +0100

    lab1/zad3: formatowanie kodu

--- lab1/zad3/sort.h
+++ lab1/zad3/sort.h
@@ -2,73 +2,73 @@
 #include "elem.h"
 
 template <class T>
-inline void _swap(T& a, T& b) { T x=a; a=b ; b=x; }
+inline void _swap(T& a, T& b) { T x = a; a = b; b = x; }
 
 template <class T>
-bool test(T tab[],int n)
-    {
-    for ( int i=1 ; i<n ; ++i )
-        if ( tab[i-1]>tab[i] ) return false;
-    return true;
-    }
+bool test(T tab[], int n)
+{
+	for (int i = 1; i<n; ++i)
+		if (tab[i - 1]>tab[i]) return false;
+	return true;
+}
 
 
-template <class T> 
-void sort1(T tab[],int n)
-    // nie zmieniac nazwy (ani sygnatury funkcji) - bedzie testowane automatycznie
-    // w komentarzu napisac jaki to algorytm
-    {
-    // implementacja pierwszego algorytmu sortowania
-    }
+template <class T>
+void sort1(T tab[], int n)
+// nie zmieniac nazwy (ani sygnatury funkcji) - bedzie testowane automatycznie
+// w komentarzu napisac jaki to algorytm
+{
+	// implementacja pierwszego algorytmu sortowania
+}
 
 
-template <class T> 
-void sort2(T tab[],int n)
-    // nie zmieniac nazwy (ani sygnatury funkcji) - bedzie testowane automatycznie
-    // w komentarzu napisac jaki to algorytm
-    {
-    // implementacja drugiego algorytmu sortowania
-    }
+template <class T>
+void sort2(T tab[], int n)
+// nie zmieniac nazwy (ani sygnatury funkcji) - bedzie testowane automatycznie
+// w komentarzu napisac jaki to algorytm
+{
+	// implementacja drugiego algorytmu sortowania
+}
 
 
-template <class T> 
-void sort3(T tab[],int n)
-    // nie zmieniac nazwy (ani sygnatury funkcji) - bedzie testowane automatycznie
-    // w komentarzu napisac jaki to algorytm
-    {
-    // implementacja trzeciego algorytmu sortowania
-    }
+template <class T>
+void sort3(T tab[], int n)
+// nie zmieniac nazwy (ani sygnatury funkcji) - bedzie testowane automatycznie
+// w komentarzu napisac jaki to algorytm
+{
+	// implementacja trzeciego algorytmu sortowania
+}
 
 
-template <class T> 
-void heapsort(T tab[],int n)
-    {
-    int i;
-    for ( i=n/2-1 ; i>=0 ; --i )
-        makeheap(tab,i,n);
-    for ( i=n-1 ; i>0 ; --i )
-        {
-        _swap(tab[0],tab[i]);
-        makeheap(tab,0,i);
-        }
-    }
+template <class T>
+void heapsort(T tab[], int n)
+{
+	int i;
+	for (i = n / 2 - 1; i >= 0; --i)
+		makeheap(tab, i, n);
+	for (i = n - 1; i > 0; --i)
+	{
+		_swap(tab[0], tab[i]);
+		makeheap(tab, 0, i);
+	}
+}
 
 // tworzy kopiec o korzeniu w elemencie tab[k]
-template <class T> 
-void makeheap(T tab[],int k,int n)
-    {
-    T x;
-    int i,j;
-    i=k;
-    j=(i<<1)+1;
-    x=tab[i];
-    while ( j<n )
-        {
-        if ( j+1<n && tab[j]<tab[j+1] ) ++j;
-        if ( x>=tab[j] ) break;
-        tab[i]=tab[j];
-        i=j;
-        j=(i<<1)+1;
-        }
-    tab[i]=x;
-    }
+template <class T>
+void makeheap(T tab[], int k, int n)
+{
+	T x;
+	int i, j;
+	i = k;
+	j = (i << 1) + 1;
+	x = tab[i];
+	while (j < n)
+	{
+		if (j + 1 < n && tab[j] < tab[j + 1]) ++j;
+		if (x >= tab[j]) break;
+		tab[i] = tab[j];
+		i = j;
+		j = (i << 1) + 1;
+	}
+	tab[i] = x;
+}
