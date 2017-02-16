5a4e19afed859135548b3c3ca2a7e99a230e8017
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-02-16 08:44:22 +0100

    lab1/zad2: formatowanie kodu

--- lab1/zad2/PriorityQueue.cs
+++ lab1/zad2/PriorityQueue.cs
@@ -5,102 +5,105 @@ using System.Collections.Generic;
 namespace ASD
 {
 
-interface IPriorityQueue
+    interface IPriorityQueue
     {
-    void Put(int p);     // wstawia element do kolejki
-    int GetMax();        // pobiera maksymalny element z kolejki (element jest usuwany z kolejki)
-    int ShowMax();       // pokazuje maksymalny element kolejki (element pozostaje w kolejce)
-    int Count { get; }   // liczba elementów kolejki
+        void Put(int p);     // wstawia element do kolejki
+        int GetMax();        // pobiera maksymalny element z kolejki (element jest usuwany z kolejki)
+        int ShowMax();       // pokazuje maksymalny element kolejki (element pozostaje w kolejce)
+        int Count { get; }   // liczba elementów kolejki
     }
 
 
-class LazyPriorityQueue : IPriorityQueue
+    class LazyPriorityQueue : IPriorityQueue
     {
 
-    public LazyPriorityQueue()
+        public LazyPriorityQueue()
         {
         }
 
-    public void Put(int p)
+        public void Put(int p)
         {
         }
 
-    public int GetMax()
+        public int GetMax()
         {
-        return 0;
-        }
-
-    public int ShowMax()
-        {
-        return 0;
-        }
-
-    public int Count
-        {
-        get {
             return 0;
+        }
+
+        public int ShowMax()
+        {
+            return 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return 0;
             }
         }
 
     } // LazyPriorityQueue
 
 
-class EagerPriorityQueue : IPriorityQueue
+    class EagerPriorityQueue : IPriorityQueue
     {
 
-    public EagerPriorityQueue()
+        public EagerPriorityQueue()
         {
         }
 
-    public void Put(int p)
+        public void Put(int p)
         {
         }
 
-    public int GetMax()
+        public int GetMax()
         {
-        return 0;
-        }
-
-    public int ShowMax()
-        {
-        return 0;
-        }
-
-    public int Count
-        {
-        get {
             return 0;
+        }
+
+        public int ShowMax()
+        {
+            return 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return 0;
             }
         }
 
     } // EagerPriorityQueue
 
 
-class HeapPriorityQueue : IPriorityQueue
+    class HeapPriorityQueue : IPriorityQueue
     {
 
-    public HeapPriorityQueue()
+        public HeapPriorityQueue()
         {
         }
 
-    public void Put(int p)
+        public void Put(int p)
         {
         }
 
-    public int GetMax()
+        public int GetMax()
         {
-        return 0;
-        }
-
-    public int ShowMax()
-        {
-        return 0;
-        }
-
-    public int Count
-        {
-        get {
             return 0;
+        }
+
+        public int ShowMax()
+        {
+            return 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return 0;
             }
         }
 
