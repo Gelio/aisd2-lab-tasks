919c0d3cba0f37c0606eff93fe0dd84cc4d9dca6
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-02-16 09:30:40 +0100

    lab1/zad2: skonczone

--- lab1/zad2/PriorityQueue.cs
+++ lab1/zad2/PriorityQueue.cs
@@ -13,6 +13,12 @@ namespace ASD
         int Count { get; }   // liczba elementów kolejki
     }
 
+    // List<T> jest implementowana za pomocą tablicy, zatem występują następujące
+    // złożności operacji:
+    // * Add - O(1) zamortyzowana, O(n) pesymistyczna gdy trzeba zwiększyć rozmiar
+    // * InsertAt - O(n) bo przesuwamy elementy
+    // * RemoveAt i RemoveAt - O(1) dla ostatniego elementu, O(n) dla pozostałych
+    // * [i] (odwołanie do elementu na i-tej pozycji - O(1)
 
     class LazyPriorityQueue : IPriorityQueue
     {
@@ -23,11 +29,15 @@ namespace ASD
             _queue = new List<int>();
         }
 
+        // O(1)
         public void Put(int p)
         {
             _queue.Add(p);
         }
 
+        // Złożność O(n), bo ShowMax ma O(n) oraz dodatkowo
+        // trzeba doliczyć przesunięcie wszystkich elementów
+        // za indeksem elementu o największej wartości przy usuwaniu
         public int GetMax()
         {
             int maxValue = ShowMax();
@@ -35,6 +45,7 @@ namespace ASD
             return maxValue;
         }
 
+        // Złożność O(n), trzeba przejrzeć całą tablicę
         public int ShowMax()
         {
             if (_queue.Count == 0)
@@ -73,6 +84,8 @@ namespace ASD
             _queue = new List<int>();
         }
 
+        // Złożność O(n) - w czasie liniowym znajdujemy miejsce dla
+        // aktualnie wstawianego elementu
         public void Put(int p)
         {
             if (_queue.Count == 0 || p <= _queue[0])
@@ -93,6 +106,8 @@ namespace ASD
             _queue.Add(p);
         }
 
+        // Złożność O(1), bo ShowMax ma O(1), usunięcie ostatniego
+        // elementu tablicy również O(1)
         public int GetMax()
         {
             int maxValue = ShowMax();
@@ -100,6 +115,7 @@ namespace ASD
             return maxValue;
         }
 
+        // O(1)
         public int ShowMax()
         {
             if (_queue.Count == 0)
@@ -121,31 +137,102 @@ namespace ASD
 
     class HeapPriorityQueue : IPriorityQueue
     {
+        // Zwykły kopiec, Max w korzeniu
+        private List<int> _queue;
 
         public HeapPriorityQueue()
         {
+            _queue = new List<int>();
         }
 
+        // Złożoność wstawiania O(log n), ponieważ najpierw dodajemy na
+        // koniec tablicy (czas stały), a następnie operacja UpHeap ma koszt
+        // logarytmiczny (przechodzi po drzewie w pionie, drzewo ma wysokość
+        // n = 2^h => h = log n
         public void Put(int p)
         {
+            _queue.Add(p);
+            UpHeap(_queue.Count - 1);
         }
 
+        // Złożoność O(log n), bo ShowMax ma O(1), a przepisanie i usunięcie
+        // ostatniego elementu ma O(1), a DownHeap O(log n) - argumentacja taka
+        // sama jak przy UpHeap w operacji Put
         public int GetMax()
         {
-            return 0;
+            int maxValue = ShowMax();
+
+            _queue[0] = _queue[_queue.Count - 1];
+            _queue.RemoveAt(_queue.Count - 1);
+
+            DownHeap(0);
+
+            return maxValue;
         }
 
+        // O(1)
         public int ShowMax()
         {
-            return 0;
+            if (_queue.Count == 0)
+                throw new InvalidOperationException();
+
+            return _queue[0];
         }
 
         public int Count
         {
             get
             {
-                return 0;
+                return _queue.Count;
             }
+        }
+
+        // O(log N)
+        private void UpHeap(int k)
+        {
+            while (k > 0 && _queue[k] > _queue[k / 2])
+            {
+                Swap(k, k / 2);
+                k /= 2;
+            }
+        }
+
+        // O(log N)
+        private void DownHeap(int k)
+        {
+            int maxIndex = _queue.Count - 1;
+
+            int leftChild = k * 2 + 1;
+            int rightChild = leftChild + 1;
+
+            while (leftChild <= maxIndex)
+            {
+                int toSwap = -1;
+                
+
+                // Sprawdzamy lewego następnika
+                if (_queue[k] < _queue[leftChild])
+                    toSwap = leftChild;
+
+                // Sprawdzamy prawego następnika o ile istnieje
+                if (rightChild <= maxIndex && _queue[k] < _queue[rightChild] && _queue[leftChild] < _queue[rightChild])
+                    toSwap = rightChild;
+
+                if (toSwap == -1)
+                    return;
+
+                Swap(k, toSwap);
+                k = toSwap;
+                leftChild = k * 2 + 1;
+                rightChild = leftChild + 1;
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            int c = _queue[i];
+            _queue[i] = _queue[j];
+            _queue[j] = c;
         }
 
     } // HeapPriorityQueue
