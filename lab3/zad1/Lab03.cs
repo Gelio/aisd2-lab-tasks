11b7467c3d766acdeae7dbecab87e1a525393b64
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-02-25 23:15:51 +0100

    lab3/zad1: code reformatting, add graph library

--- lab3/zad1/Lab03.cs
+++ lab3/zad1/Lab03.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using ASD.Graph;
+using ASD.Graphs;
 
 class Lab03
-    {
+{
 
     /// <summary>
     /// Konstruowanie grafu krawedziowego
@@ -14,39 +14,39 @@ class Lab03
     /// Wierzcholek grafu krawedziowego odpowiadajacy krawedzi <u,v> grafu pierwotnego ma opis "u-v"
     /// Czyli np. dla krawedzi <1,2> powinno byc "1-2".
     /// </remarks>
-    public static IGraph LineGraph(IGraph graph, out string[] desc)
+    public static Graph LineGraph(Graph graph, out string[] desc)
+    {
+        Graph t = new AdjacencyMatrixGraph(false, graph.EdgesCount);
+        int[,] vert = new int[graph.VerticesCount, graph.VerticesCount];
+        for (int i = 0; i < graph.VerticesCount; i++) for (int j = 0; j < graph.VerticesCount; j++) vert[i, j] = -1;
+        int k = 0;
+        string[] description = new string[graph.EdgesCount];
+
+        for (int v = 0; v < graph.VerticesCount; ++v)
         {
-            IGraph t = new AdjacencyMatrixGraph(false, graph.EdgesCount);
-            int[,] vert = new int[graph.VerticesCount,graph.VerticesCount];
-            for (int i = 0; i < graph.VerticesCount; i++) for (int j = 0; j < graph.VerticesCount; j++) vert[i, j] = -1;
-            int k = 0;
-            string[] description = new string[graph.EdgesCount];
 
-            for ( int v=0 ; v<graph.VerticesCount ; ++v )
-            {
+            foreach (Edge e1 in graph.OutEdges(v))
+                if (vert[e1.From, e1.To] == -1)
+                {
+                    description[k] = e1.From.ToString() + "-" + e1.To.ToString();
+                    vert[e1.From, e1.To] = vert[e1.To, e1.From] = k++;
 
-                foreach (Edge e1 in graph.OutEdges(v))
-                    if (vert[e1.From, e1.To] == -1)
-                    {
-                        description[k] = e1.From.ToString() + "-" + e1.To.ToString();
-                        vert[e1.From, e1.To] = vert[e1.To, e1.From] = k++;
-
-                    }
-            }
-
-            for (int v = 0; v < graph.VerticesCount; ++v)
-            {
-
-                foreach (Edge e1 in graph.OutEdges(v))
-                    foreach (Edge e2 in graph.OutEdges(e1.To))
-                        if (vert[e1.From, e1.To] != vert[e2.From, e2.To])
-                            t.AddEdge(vert[e1.From, e1.To], vert[e2.From, e2.To]);
-                         
-            }
-        
-        desc=description;     // zmienic
-        return t;  // zmienic (wynikiem ma byc calkiem inny graf !!!)
+                }
         }
+
+        for (int v = 0; v < graph.VerticesCount; ++v)
+        {
+
+            foreach (Edge e1 in graph.OutEdges(v))
+                foreach (Edge e2 in graph.OutEdges(e1.To))
+                    if (vert[e1.From, e1.To] != vert[e2.From, e2.To])
+                        t.AddEdge(vert[e1.From, e1.To], vert[e2.From, e2.To]);
+
+        }
+
+        desc = description;     // zmienic
+        return t;  // zmienic (wynikiem ma byc calkiem inny graf !!!)
+    }
 
     /// <summary>
     /// Sortowanie topologiczne z wykorzystaniem wierzchołków o stopniu wejściowym równym zero
@@ -58,33 +58,33 @@ class Lab03
     /// Wowczas: ord[i] to numer wierzcholka i w porzadku topologicznym
     /// Nie wolno zmianiac zadanego grafu !!!
     /// </remarks>
-    public static int[] TopologicalSort_V0(IGraph graph)
+    public static int[] TopologicalSort_V0(Graph graph)
+    {
+        Graph t = graph.Clone();
+        int[] ord = new int[graph.VerticesCount];
+        for (int i = 0; i < ord.Length; i++) ord[i] = -1;
+
+        int key = 0;
+
+        while (key < t.VerticesCount)
         {
-            IGraph t = graph.Clone();
-            int[] ord = new int[graph.VerticesCount];            
-            for (int i = 0; i < ord.Length; i++) ord[i] = -1;           
+            bool deleted = false;
+            for (int v = 0; v < t.VerticesCount; ++v)
+                if (t.InDegree(v) == 0 && ord[v] == -1)
+                {
+                    ord[v] = key++;
+                    foreach (Edge e in t.OutEdges(v)) t.DelEdge(e);
+                    deleted = true;
+                }
 
-            int key = 0;
+            if (deleted == false) return null;
+        }
 
-            while(key < t.VerticesCount)
-            {
-                bool deleted = false;
-                for (int v = 0; v < t.VerticesCount; ++v)
-                    if (t.InDegree(v) == 0 && ord[v] == -1)
-                    {
-                        ord[v] = key++;
-                        foreach (Edge e in t.OutEdges(v)) t.DelEdge(e);
-                        deleted = true;
-                    }
-                
-                if (deleted == false) return null;
-            }
-            
-            
+
         //for(int i = 0; i < ord.Length; i++) Console.WriteLine("{0}. {1}",i, ord[i]);
 
         return ord;  // zmienic
-        }
+    }
 
     /// <summary>
     /// Sortowanie topologiczne z uzyciem DFS
@@ -96,9 +96,9 @@ class Lab03
     /// Wowczas: ord[i] to numer wierzcholka i w porzadku topologicznym
     /// Nie wolno zmianiac zadanego grafu !!!
     /// </remarks>
-    public static int[] TopologicalSort_DFS(IGraph graph)
-        {
+    public static int[] TopologicalSort_DFS(Graph graph)
+    {
         return null;  // zmienic
-        }
+    }
 }
 
--- lab3/zad1/Program.cs
+++ lab3/zad1/Program.cs
@@ -1,19 +1,19 @@
-﻿
+
 using System;
-using ASD.Graph;
+using ASD.Graphs;
 
 class Test
 {
 
     public static void Main()
-        {
+    {
         var ge = new GraphExport();
         var rgg = new RandomGraphGenerator(123);
         int[] order;
         string[] desc;
-        int[] ec =new int[3];
+        int[] ec = new int[3];
 
-        IGraph g = new AdjacencyMatrixGraph(false, 5);
+        Graph g = new AdjacencyMatrixGraph(false, 5);
         g.AddEdge(0, 1);
         g.AddEdge(0, 4);
         g.AddEdge(1, 2);
@@ -21,34 +21,34 @@ class Test
         g.AddEdge(2, 3);
         g.AddEdge(3, 4);
         g.AddEdge(4, 3);
-        ge.Export(g,null,"G");
+        ge.Export(g, null, "G");
 
-        IGraph lg = Lab03.LineGraph(g,out desc);
-        ge.Export(lg,desc,"LG");
+        Graph lg = Lab03.LineGraph(g, out desc);
+        ge.Export(lg, desc, "LG");
 
-        IGraph[] g2 = new IGraph[3]; 
+        Graph[] g2 = new Graph[3];
         g2[0] = new AdjacencyMatrixGraph(true, 4);
         g2[0].AddEdge(0, 1);
         g2[0].AddEdge(0, 3);
         g2[0].AddEdge(1, 2);
         g2[0].AddEdge(3, 2);
-       
-        
-        g2[1] = rgg.DAG(typeof(AdjacencyMatrixGraph),100,0.9,1,1);
-        g2[2] = rgg.DAG(typeof(AdjacencyListsGraph),1000,0.2,1,1);
+
+
+        g2[1] = rgg.DAG(typeof(AdjacencyMatrixGraph), 100, 0.9, 1, 1);
+        g2[2] = rgg.DAG(typeof(AdjacencyListsGraph<SimpleAdjacencyList>), 1000, 0.2, 1, 1);
 
         ec[0] = g2[0].EdgesCount;
         ec[1] = g2[1].EdgesCount;
         ec[2] = g2[2].EdgesCount;
 
         Console.WriteLine("Sortowanie topologiczne - DFS");
-        for ( int i=0 ; i<3 ; ++i )
-            {
+        for (int i = 0; i < 3; ++i)
+        {
             order = Lab03.TopologicalSort_DFS(g2[i]);
-            Console.WriteLine("  test {0} : {1}",i,TopologicalSortTest(g2[i],order));
-            }
+            Console.WriteLine("  test {0} : {1}", i, TopologicalSortTest(g2[i], order));
+        }
 
-        if ( ec[0]!=g2[0].EdgesCount || ec[1]!=g2[1].EdgesCount ||ec[2]!=g2[2].EdgesCount )
+        if (ec[0] != g2[0].EdgesCount || ec[1] != g2[1].EdgesCount || ec[2] != g2[2].EdgesCount)
             Console.WriteLine("  Blad - zmieniono graf");
 
         ec[0] = g2[0].EdgesCount;
@@ -56,46 +56,46 @@ class Test
         ec[2] = g2[2].EdgesCount;
 
         Console.WriteLine("Sortowanie topologiczne - zrodla 1");
-        for ( int i=0 ; i<3 ; ++i )
-            {
+        for (int i = 0; i < 3; ++i)
+        {
             order = Lab03.TopologicalSort_V0(g2[i]);
-            Console.WriteLine("  test {0} : {1}",i,TopologicalSortTest(g2[i],order));
-            }
+            Console.WriteLine("  test {0} : {1}", i, TopologicalSortTest(g2[i], order));
+        }
 
-        if ( ec[0]!=g2[0].EdgesCount || ec[1]!=g2[1].EdgesCount ||ec[2]!=g2[2].EdgesCount )
+        if (ec[0] != g2[0].EdgesCount || ec[1] != g2[1].EdgesCount || ec[2] != g2[2].EdgesCount)
             Console.WriteLine("  Blad - zmieniono graf");
 
-        IGraph[] g3 = new IGraph[3];
+        Graph[] g3 = new Graph[3];
         g3[0] = g2[0].Clone();
-        g3[0].AddEdge(1,0);
-        g3[1] = rgg.DirectedGraph(typeof(AdjacencyMatrixGraph),100,0.9,1,1);
-        g3[2] = rgg.DirectedGraph(typeof(AdjacencyListsGraph),1000,0.2,1,1);
-        
+        g3[0].AddEdge(1, 0);
+        g3[1] = rgg.DirectedGraph(typeof(AdjacencyMatrixGraph), 100, 0.9, 1, 1);
+        g3[2] = rgg.DirectedGraph(typeof(AdjacencyListsGraph<SimpleAdjacencyList>), 1000, 0.2, 1, 1);
+
         ec[0] = g3[0].EdgesCount;
         ec[1] = g3[1].EdgesCount;
         ec[2] = g3[2].EdgesCount;
 
         Console.WriteLine("Sortowanie topologiczne - zrodla 2");
-        for ( int i=0 ; i<3 ; ++i )
-            {
+        for (int i = 0; i < 3; ++i)
+        {
             order = Lab03.TopologicalSort_V0(g3[i]);
-            Console.WriteLine("  test {0} : {1}",i,order==null);
-            }
+            Console.WriteLine("  test {0} : {1}", i, order == null);
+        }
 
-        if ( ec[0]!=g3[0].EdgesCount || ec[1]!=g3[1].EdgesCount || ec[2]!=g3[2].EdgesCount )
+        if (ec[0] != g3[0].EdgesCount || ec[1] != g3[1].EdgesCount || ec[2] != g3[2].EdgesCount)
             Console.WriteLine("  Blad - zmieniono graf");
 
-        }
+    }
 
-    static bool TopologicalSortTest(IGraph g, int[] ord)
-        {
+    static bool TopologicalSortTest(Graph g, int[] ord)
+    {
 
-        if ( ord==null ) return false;
-        for ( int v=0 ; v<g.VerticesCount ; ++v )
-            foreach ( var e in g.OutEdges(v) )
-                if ( ord[e.From]>=ord[e.To] )
+        if (ord == null) return false;
+        for (int v = 0; v < g.VerticesCount; ++v)
+            foreach (var e in g.OutEdges(v))
+                if (ord[e.From] >= ord[e.To])
                     return false;
         return true;
-        }
+    }
 
 }
