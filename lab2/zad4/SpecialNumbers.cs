b74a0c1a4ec03818c90b94c5cfd1ccd9a1df02a5
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-02-28 11:54:50 +0100

    lab2/zad4: complete dynamic programming with additional requirements solution

--- lab2/zad4/SpecialNumbers.cs
+++ lab2/zad4/SpecialNumbers.cs
@@ -74,7 +74,32 @@ namespace ASD
         // req - tablica z wymaganiami, jezeli req[i, j] == 0 to znaczy, ze  i + 1 nie moze stac PRZED j + 1
         public static int SpecialNumbersDP(int n, bool[,] req)
         {
-            // ZMIEN
+            if (n == 0)
+                return 0;
+            int[,] countWithLeadingDigit = new int[n + 1, 10];
+            for (int i = 1; i <= 9; i++)
+                countWithLeadingDigit[1, i] = 1;
+
+            for (int numberLength = 2; numberLength <= n; numberLength++)
+            {
+                for (int digit = 1; digit <= 9; digit++)
+                {
+                    for (int nextDigit = 1; nextDigit <= digit; nextDigit++)
+                    {
+                        if (req[digit - 1, nextDigit - 1])
+                        {
+                            countWithLeadingDigit[numberLength, digit] += countWithLeadingDigit[numberLength - 1, nextDigit];
+                            countWithLeadingDigit[numberLength, digit] %= mod;
+                        }
+                    }
+                }
+            }
+
+            int lastRowCount = 0;
+            for (int i = 1; i <= 9; i++)
+                lastRowCount += countWithLeadingDigit[n, i];
+
+            return lastRowCount % mod;
             return 0;
         }
 
