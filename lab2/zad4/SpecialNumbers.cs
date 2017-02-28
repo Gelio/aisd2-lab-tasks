b09385df2674c55fdb50215d0f74f9722c3fc762
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-02-28 11:41:21 +0100

    lab2/zad4: complete recursive part

--- lab2/zad4/SpecialNumbers.cs
+++ lab2/zad4/SpecialNumbers.cs
@@ -11,9 +11,29 @@ namespace ASD
         // n cyfr
         public static int SpecialNumbersRec(int n)
         {
-            // ZMIEN
-            // mozesz dopisac swoja rekurencyjna funkcje pomocnicza
-            return 0;
+            if (n == 0)
+                return 0;
+            return SpecialNumbersRecursiveInternal(n, 9, -1);
+        }
+
+        private static int SpecialNumbersRecursiveInternal(int n, int maxDigit, int previousParity)
+        {
+            if (n == 0)
+                return 1;
+
+            if (maxDigit <= 1)
+                return maxDigit;
+
+            int totalCount = 0;
+            for (int i = 1; i <= maxDigit; i++)
+            {
+                int currentParity = i % 2;
+                if (i == maxDigit || currentParity != previousParity)
+                    totalCount += SpecialNumbersRecursiveInternal(n - 1, i, currentParity);
+                totalCount %= mod;
+            }
+
+            return totalCount;
         }
 
         // programowanie dynamiczne
