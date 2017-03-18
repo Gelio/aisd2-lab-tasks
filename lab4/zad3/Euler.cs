b2963c19487c49d71985f141808300ed97e19ade
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-03-18 11:21:33 +0100

    lab4/zad3: create project, adjust function calls to the newest Graph library

--- lab4/zad3/Euler.cs
+++ lab4/zad3/Euler.cs
@@ -1,58 +1,58 @@
 
-namespace ASD.Graph
+namespace ASD.Graphs
 {
-using System.Linq;  // potrzebne dla metody First rozszerzającej interfejs IEnumerable
-
-/// <summary>
-/// Rozszerzenie interfejsu <see cref="IGraph"/> o wyszukiwanie ścieżki Eulera
-/// </summary>
-public static class EulerPathGraphExtender
-    {
+    using System.Linq;  // potrzebne dla metody First rozszerzającej interfejs IEnumerable
 
     /// <summary>
-    /// Znajduje scieżkę Eulera w grafie
+    /// Rozszerzenie interfejsu <see cref="IGraph"/> o wyszukiwanie ścieżki Eulera
     /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <param name="ec">Znaleziona ścieżka (parametr wyjściowy)</param>
-    /// <returns>Informacja czy ścieżka Eulera istnieje</returns>
-    /// <remarks>
-    /// Jeśli w badanym grafie nie istnieje ścieżka Eulera metoda zwraca <b>false</b>, parametr <i>ec</i> ma wówczas wartość <b>null</b>.<br/>
-    /// <br/>
-    /// Metoda nie modyfikuje badanego grafu.<br/>
-    /// <br/>
-    /// Metoda implementuje algorytm Fleury'ego.
-    /// </remarks>
-    public static bool Lab04_Euler(this IGraph g, out Edge[] ec)
+    public static class EulerPathGraphExtender
+    {
+
+        /// <summary>
+        /// Znajduje scieżkę Eulera w grafie
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="ec">Znaleziona ścieżka (parametr wyjściowy)</param>
+        /// <returns>Informacja czy ścieżka Eulera istnieje</returns>
+        /// <remarks>
+        /// Jeśli w badanym grafie nie istnieje ścieżka Eulera metoda zwraca <b>false</b>, parametr <i>ec</i> ma wówczas wartość <b>null</b>.<br/>
+        /// <br/>
+        /// Metoda nie modyfikuje badanego grafu.<br/>
+        /// <br/>
+        /// Metoda implementuje algorytm Fleury'ego.
+        /// </remarks>
+        public static bool Lab04_Euler(this Graph g, out Edge[] ec)
         {
-        // tylko cykl     - 2 pkt
-        // cykl i sciezka - 3 pkt
+            // tylko cykl     - 2 pkt
+            // cykl i sciezka - 3 pkt
 
-        /*
-        Algorytm Fleury'ego
+            /*
+            Algorytm Fleury'ego
 
-        utworz pusty stos krawedzi Euler
-        utworz pusty stos krawedzi pom
-        w = dowolny wierzcholek grafu
-        umiesc na stosie pom sztuczna krawedz <w,w>
-        dopoki pom jest niepusty powtarzaj
-            w = wierzch. koncowy krawedzi ze szczytu stosu pom (bez pobierania krawedzi ze stosu)
-            jesli stopien wychodzacy w > 0 
-                e = dowolna krawedz wychodzaca z w
-                umiesc krawedz e na stosie pom      
-                usun krawedz e z grafu
-            w przeciwnym przypadku
-                pobiez szczytowy element ze stosu pom i umiesc go na stosie Euler
-        usun ze stosu Euler sztuczna krawedz (petle) startowa (jest na szczycie)
+            utworz pusty stos krawedzi Euler
+            utworz pusty stos krawedzi pom
+            w = dowolny wierzcholek grafu
+            umiesc na stosie pom sztuczna krawedz <w,w>
+            dopoki pom jest niepusty powtarzaj
+                w = wierzch. koncowy krawedzi ze szczytu stosu pom (bez pobierania krawedzi ze stosu)
+                jesli stopien wychodzacy w > 0 
+                    e = dowolna krawedz wychodzaca z w
+                    umiesc krawedz e na stosie pom      
+                    usun krawedz e z grafu
+                w przeciwnym przypadku
+                    pobiez szczytowy element ze stosu pom i umiesc go na stosie Euler
+            usun ze stosu Euler sztuczna krawedz (petle) startowa (jest na szczycie)
 
-        wynik: krawedzie tworzace cykl sa na stosie Euler
-        
-        Uwaga: powyzszy algorytm znajduje cykl Eulera (jesli istnieje),
-               aby znalezc sciezke nalezy najpierw wyznaczyc wierzcholek startowy
-               (nie mozna wystartowac z dowolnego)
-        */
+            wynik: krawedzie tworzace cykl sa na stosie Euler
 
-        ec=null;
-        return false;
+            Uwaga: powyzszy algorytm znajduje cykl Eulera (jesli istnieje),
+                   aby znalezc sciezke nalezy najpierw wyznaczyc wierzcholek startowy
+                   (nie mozna wystartowac z dowolnego)
+            */
+
+            ec = null;
+            return false;
         }
 
     }  // class EulerGraphExtender
--- lab4/zad3/Kruskal.cs
+++ lab4/zad3/Kruskal.cs
@@ -1,32 +1,30 @@
 
-namespace ASD.Graph
+namespace ASD.Graphs
 {
 
-/// <summary>
-/// Rozszerzenie interfejsu <see cref="IGraph"/> o wyznaczanie minimalnego drzewa rozpinającego algorytmem Kruskala
-/// </summary>
-public static class KruskalGraphExtender
+    /// <summary>
+    /// Rozszerzenie interfejsu <see cref="IGraph"/> o wyznaczanie minimalnego drzewa rozpinającego algorytmem Kruskala
+    /// </summary>
+    public static class KruskalGraphExtender
     {
 
-    /// <summary>
-    /// Wyznacza minimalne drzewo rozpinające grafu algorytmem Kruskala
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <param name="mst">Wyznaczone drzewo rozpinające (parametr wyjściowy)</param>
-    /// <returns>Waga minimalnego drzewa rozpinającego</returns>
-    /// <remarks>
-    /// Dla grafu skierowanego metoda zgłasza wyjątek <see cref="System.ArgumentException"/>.<br/>
-    /// Wyznaczone drzewo reprezentowane jast jako graf bez cykli, to umożliwia jednolitą obsługę sytuacji
-    /// gdy analizowany graf jest niespójny, wyzmnaczany jest wówczas las rozpinający.
-    /// </remarks>
-    public static int Lab04_Kruskal(this IGraph g, out IGraph mst)
+        /// <summary>
+        /// Wyznacza minimalne drzewo rozpinające grafu algorytmem Kruskala
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="mst">Wyznaczone drzewo rozpinające (parametr wyjściowy)</param>
+        /// <returns>Waga minimalnego drzewa rozpinającego</returns>
+        /// <remarks>
+        /// Dla grafu skierowanego metoda zgłasza wyjątek <see cref="System.ArgumentException"/>.<br/>
+        /// Wyznaczone drzewo reprezentowane jast jako graf bez cykli, to umożliwia jednolitą obsługę sytuacji
+        /// gdy analizowany graf jest niespójny, wyzmnaczany jest wówczas las rozpinający.
+        /// </remarks>
+        public static int Lab04_Kruskal(this Graph g, out Graph mst)
         {
-        // 1 pkt
+            // 1 pkt
 
-        // wykorzystac klase UnionFind z biblioteki Graph
-
-        mst=g;
-        return 0;
+            mst = g;
+            return 0;
         }
 
     }  // class KruskalGraphExtender
