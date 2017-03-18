3daf02f99804027990c9010e322e3a26d646c99e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-03-18 12:05:02 +0100

    lab5/zad3: code formatting

--- lab5/zad3/Lab05.cs
+++ lab5/zad3/Lab05.cs
@@ -2,101 +2,101 @@
 namespace ASD.Graphs
 {
 
-public static class Lab05GraphExtender
+    public static class Lab05GraphExtender
     {
 
-    /// <summary>
-    /// Wyznacza silnie spójne składowe
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <param name="scc">Silnie spójne składowe (parametr wyjściowy)</param>
-    /// <returns>Liczba silnie spójnych składowych</returns>
-    /// <remarks>
-    /// scc[v] = numer silnie spójnej składowej do której należy wierzchołek v<br/>
-    /// (numerujemy od 0)<br/>
-    /// <br/>
-    /// Metoda uruchomiona dla grafu nieskierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
-    /// <br/>
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// </remarks>
-    public static int StronglyConnectedComponents(this Graph g, out int[] scc)
+        /// <summary>
+        /// Wyznacza silnie spójne składowe
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="scc">Silnie spójne składowe (parametr wyjściowy)</param>
+        /// <returns>Liczba silnie spójnych składowych</returns>
+        /// <remarks>
+        /// scc[v] = numer silnie spójnej składowej do której należy wierzchołek v<br/>
+        /// (numerujemy od 0)<br/>
+        /// <br/>
+        /// Metoda uruchomiona dla grafu nieskierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
+        /// <br/>
+        /// Graf wejściowy pozostaje niezmieniony.
+        /// </remarks>
+        public static int StronglyConnectedComponents(this Graph g, out int[] scc)
         {
-        scc=new int[g.VerticesCount];
-        return 1;
-        }
- 
-    /// <summary>
-    /// Wyznacza odwrotność grafu
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <returns>Odwrotność grafu</returns>
-    /// <remarks>
-    /// Odwrotność grafu to graf skierowany o wszystkich krawędziach przeciwnie skierowanych niż w grafie pierwotnym.<br/>
-    /// <br/>
-    /// Metoda uruchomiona dla grafu nieskierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
-    /// <br/>
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// </remarks>
-    public static Graph Reverse(this Graph g)
-        {
-        return g;
+            scc = new int[g.VerticesCount];
+            return 1;
         }
 
-    /// <summary>
-    /// Wyznacza jądro grafu
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <returns>Jądro grafu</returns>
-    /// <remarks>
-    /// Jądro grafu to graf skierowany, którego wierzchołkami są silnie spójne składowe pierwotnego grafu.<br/>
-    /// Cała silnie spójna składowa jest "ściśnięta" do jednego wierzchiłka.<br/>
-    /// Wierzchołki jądra są połączone krawędzią gdy w grafie pierwotnym połączone są krawędzią dowolne
-    /// z wierzchołków odpowiednich składowych (ale nie wprowadzamy pętli !). Wagi krawędzi przyjmujemy równe 1.<br/>
-    /// <br/>
-    /// Metoda uruchomiona dla grafu nieskierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
-    /// <br/>
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// </remarks>
-    public static Graph Kernel(this Graph g)
+        /// <summary>
+        /// Wyznacza odwrotność grafu
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <returns>Odwrotność grafu</returns>
+        /// <remarks>
+        /// Odwrotność grafu to graf skierowany o wszystkich krawędziach przeciwnie skierowanych niż w grafie pierwotnym.<br/>
+        /// <br/>
+        /// Metoda uruchomiona dla grafu nieskierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
+        /// <br/>
+        /// Graf wejściowy pozostaje niezmieniony.
+        /// </remarks>
+        public static Graph Reverse(this Graph g)
         {
-        return g;
+            return g;
         }
 
-    /// <summary>
-    /// Wyznacza ścieżki o maksymalnej przepustowości
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <param name="s">Wierzchołek źródłowy</param>
-    /// <param name="d">Znalezione ścieżki (parametr wyjściowy)</param>
-    /// <returns><b>true</b></returns>
-    /// <remarks>
-    /// Metoda przydaje się w algorytmach wyznaczania maksymalnego przepływu, wagi krawędzi oznaczają tu przepustowość krawędzi.<br/>
-    /// Przepustowość ścieżki to najmniejsza z przepustowości krawędzi wchodzących w jej skład.<br/>
-    /// <br/>
-    /// Elementy tablicy <i>d</i> zawierają przepustowości ścieżek od źródła do wierzchołka określonego przez indeks elementu.<br/>
-    /// Jeśli ścieżka od źródła do danego wierzchołka nie istnieje, to przepustowość ma wartość <b>null</b>.
-    /// <br/>
-    /// Metoda zawsze zwraca <b>true</b> (można ją stosować do każdego grafu).
-    /// </remarks>
-    public static bool MaxFlowPathsLab05(this Graph g, int s, out PathsInfo[] d)
+        /// <summary>
+        /// Wyznacza jądro grafu
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <returns>Jądro grafu</returns>
+        /// <remarks>
+        /// Jądro grafu to graf skierowany, którego wierzchołkami są silnie spójne składowe pierwotnego grafu.<br/>
+        /// Cała silnie spójna składowa jest "ściśnięta" do jednego wierzchiłka.<br/>
+        /// Wierzchołki jądra są połączone krawędzią gdy w grafie pierwotnym połączone są krawędzią dowolne
+        /// z wierzchołków odpowiednich składowych (ale nie wprowadzamy pętli !). Wagi krawędzi przyjmujemy równe 1.<br/>
+        /// <br/>
+        /// Metoda uruchomiona dla grafu nieskierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
+        /// <br/>
+        /// Graf wejściowy pozostaje niezmieniony.
+        /// </remarks>
+        public static Graph Kernel(this Graph g)
         {
-        d = new PathsInfo[g.VerticesCount];
-        return true;
+            return g;
         }
 
-    /// <summary>
-    /// Bada czy graf nieskierowany jest acykliczny
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <returns>Informacja czy graf jest acykliczny</returns>
-    /// <remarks>
-    /// Metoda uruchomiona dla grafu skierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
-    /// <br/>
-    /// Graf wejściowy pozostaje niezmieniony.
-    /// </remarks>
-    public static bool IsUndirectedAcyclic(this Graph g)
+        /// <summary>
+        /// Wyznacza ścieżki o maksymalnej przepustowości
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="s">Wierzchołek źródłowy</param>
+        /// <param name="d">Znalezione ścieżki (parametr wyjściowy)</param>
+        /// <returns><b>true</b></returns>
+        /// <remarks>
+        /// Metoda przydaje się w algorytmach wyznaczania maksymalnego przepływu, wagi krawędzi oznaczają tu przepustowość krawędzi.<br/>
+        /// Przepustowość ścieżki to najmniejsza z przepustowości krawędzi wchodzących w jej skład.<br/>
+        /// <br/>
+        /// Elementy tablicy <i>d</i> zawierają przepustowości ścieżek od źródła do wierzchołka określonego przez indeks elementu.<br/>
+        /// Jeśli ścieżka od źródła do danego wierzchołka nie istnieje, to przepustowość ma wartość <b>null</b>.
+        /// <br/>
+        /// Metoda zawsze zwraca <b>true</b> (można ją stosować do każdego grafu).
+        /// </remarks>
+        public static bool MaxFlowPathsLab05(this Graph g, int s, out PathsInfo[] d)
         {
-        return false;
+            d = new PathsInfo[g.VerticesCount];
+            return true;
+        }
+
+        /// <summary>
+        /// Bada czy graf nieskierowany jest acykliczny
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <returns>Informacja czy graf jest acykliczny</returns>
+        /// <remarks>
+        /// Metoda uruchomiona dla grafu skierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.
+        /// <br/>
+        /// Graf wejściowy pozostaje niezmieniony.
+        /// </remarks>
+        public static bool IsUndirectedAcyclic(this Graph g)
+        {
+            return false;
         }
 
     }  // class Lab05GraphExtender
--- lab5/zad3/Lab05_main.cs
+++ lab5/zad3/Lab05_main.cs
@@ -3,137 +3,137 @@ using System;
 using ASD.Graphs;
 
 class Example
-    {
+{
 
-    public static void Main ()
-        {
+    public static void Main()
+    {
         var ge = new GraphExport();
         var rgg = new RandomGraphGenerator();
 
-        Graph g1= new AdjacencyMatrixGraph(true,15);
-        g1.AddEdge(0,2);
-        g1.AddEdge(0,7);
-        g1.AddEdge(1,0);
-        g1.AddEdge(2,1);
-        g1.AddEdge(2,3);
-        g1.AddEdge(2,7);
-        g1.AddEdge(3,5);
-        g1.AddEdge(4,3);
-        g1.AddEdge(5,4);
-        g1.AddEdge(5,6);
-        g1.AddEdge(6,7);
-        g1.AddEdge(7,6);
-        g1.AddEdge(7,8);
-        g1.AddEdge(8,9);
-        g1.AddEdge(10,0);
-        g1.AddEdge(12,13);
-        g1.AddEdge(13,14);
-        g1.AddEdge(14,12);
+        Graph g1 = new AdjacencyMatrixGraph(true, 15);
+        g1.AddEdge(0, 2);
+        g1.AddEdge(0, 7);
+        g1.AddEdge(1, 0);
+        g1.AddEdge(2, 1);
+        g1.AddEdge(2, 3);
+        g1.AddEdge(2, 7);
+        g1.AddEdge(3, 5);
+        g1.AddEdge(4, 3);
+        g1.AddEdge(5, 4);
+        g1.AddEdge(5, 6);
+        g1.AddEdge(6, 7);
+        g1.AddEdge(7, 6);
+        g1.AddEdge(7, 8);
+        g1.AddEdge(8, 9);
+        g1.AddEdge(10, 0);
+        g1.AddEdge(12, 13);
+        g1.AddEdge(13, 14);
+        g1.AddEdge(14, 12);
         ge.Export(g1);
 
         int[] scc;
         int n;
-        n=g1.StronglyConnectedComponents(out scc);
-        Console.WriteLine("\nLiczba silnie spojnych skladowych: {0} (powinno byc 8)",n);
-        for ( int c=0 ; c<n ; ++c )
-            {
+        n = g1.StronglyConnectedComponents(out scc);
+        Console.WriteLine("\nLiczba silnie spojnych skladowych: {0} (powinno byc 8)", n);
+        for (int c = 0; c < n; ++c)
+        {
             Console.WriteLine();
-            Console.Write("skladowa {0}:",c);
-            for ( int v=0 ; v<g1.VerticesCount ; ++v )
-                if ( scc[v]==c )
-                    Console.Write(" {0}",v);
-            }
+            Console.Write("skladowa {0}:", c);
+            for (int v = 0; v < g1.VerticesCount; ++v)
+                if (scc[v] == c)
+                    Console.Write(" {0}", v);
+        }
         Console.WriteLine();
 
         Graph k1 = g1.Kernel();
         ge.Export(k1);
 
         rgg.SetSeed(500);
-        Graph g2=rgg.DirectedGraph(typeof(AdjacencyMatrixGraph),1000,0.005);
-        n=g2.StronglyConnectedComponents(out scc);
-        Console.WriteLine("\nLiczba silnie spojnych skladowych: {0} (powinno byc 17)",n);
+        Graph g2 = rgg.DirectedGraph(typeof(AdjacencyMatrixGraph), 1000, 0.005);
+        n = g2.StronglyConnectedComponents(out scc);
+        Console.WriteLine("\nLiczba silnie spojnych skladowych: {0} (powinno byc 17)", n);
 
-        Graph g3=rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph),1000,0.1);
+        Graph g3 = rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph), 1000, 0.1);
         try
-            {
-            n=g3.StronglyConnectedComponents(out scc);
+        {
+            n = g3.StronglyConnectedComponents(out scc);
             Console.WriteLine("\nBlad - powinien byc wyjatek");
-            }
-        catch ( ArgumentException e)
-            {
-            Console.WriteLine("\n"+e.Message+"  - dobrze");
-            }
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine("\n" + e.Message + "  - dobrze");
+        }
 
         Console.WriteLine("\nSciezka maksymalnie powiekszajaca");
         PathsInfo[] d;
         bool b;
-        Graph m1= new AdjacencyMatrixGraph(true,7);
-        m1.AddEdge(0,2,10);
-        m1.AddEdge(2,3,4);
-        m1.AddEdge(2,5,3);
-        m1.AddEdge(2,4,7);
-        m1.AddEdge(3,1,2);
-        m1.AddEdge(4,1,1);
-        m1.AddEdge(5,6,5);
-        m1.AddEdge(6,1,4);
+        Graph m1 = new AdjacencyMatrixGraph(true, 7);
+        m1.AddEdge(0, 2, 10);
+        m1.AddEdge(2, 3, 4);
+        m1.AddEdge(2, 5, 3);
+        m1.AddEdge(2, 4, 7);
+        m1.AddEdge(3, 1, 2);
+        m1.AddEdge(4, 1, 1);
+        m1.AddEdge(5, 6, 5);
+        m1.AddEdge(6, 1, 4);
         ge.Export(m1);
 
-        b=m1.MaxFlowPathsLab05(2, out d);
+        b = m1.MaxFlowPathsLab05(2, out d);
         Console.WriteLine("graf m1");
-        for ( int v=0 ; v<m1.VerticesCount ; ++v )
-            Console.WriteLine("przepustowosc od 2 do {0} wynosi {1}",v,d[v].Dist.IsNaN() ? 0 : d[v].Dist);
+        for (int v = 0; v < m1.VerticesCount; ++v)
+            Console.WriteLine("przepustowosc od 2 do {0} wynosi {1}", v, d[v].Dist.IsNaN() ? 0 : d[v].Dist);
 
-        int[] p = {55,33,65,73,0,73,84,76,45,78};
+        int[] p = { 55, 33, 65, 73, 0, 73, 84, 76, 45, 78 };
         rgg.SetSeed(200);
-        Graph m2= rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph),200,0.02,1,99);
-        b=m2.MaxFlowPathsLab05(5, out d);
+        Graph m2 = rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph), 200, 0.02, 1, 99);
+        b = m2.MaxFlowPathsLab05(5, out d);
         Console.WriteLine("graf m2");
-        for ( int v=10 ; v<20 ; ++v )
-            Console.WriteLine("przepustowosc od 5 do do {0} wynosi {1} ({2})",v, d[v].Dist.IsNaN() ? 0 : d[v].Dist, (d[v].Dist.IsNaN() ? 0 : d[v].Dist) ==p[v-10]?"OK":"blad");
+        for (int v = 10; v < 20; ++v)
+            Console.WriteLine("przepustowosc od 5 do do {0} wynosi {1} ({2})", v, d[v].Dist.IsNaN() ? 0 : d[v].Dist, (d[v].Dist.IsNaN() ? 0 : d[v].Dist) == p[v - 10] ? "OK" : "blad");
 
         Console.WriteLine("\nTesty Acyklicznosci\n");
         Graph a1, a2, a3, a4, a5;
-        bool b1,b2,b3,b4,b5;
+        bool b1, b2, b3, b4, b5;
 
         rgg.SetSeed(101);
-        a1=rgg.TreeGraph(typeof(AdjacencyListsGraph<AVLAdjacencyList>),10,1);
-//        ge.Export(a1,"a1");
-        b1=a1.IsUndirectedAcyclic();
-        Console.WriteLine("Czy graf a1 jest acykliczny ? : {0} (powinno byc True)",b1);
+        a1 = rgg.TreeGraph(typeof(AdjacencyListsGraph<AVLAdjacencyList>), 10, 1);
+        //        ge.Export(a1,"a1");
+        b1 = a1.IsUndirectedAcyclic();
+        Console.WriteLine("Czy graf a1 jest acykliczny ? : {0} (powinno byc True)", b1);
 
         rgg.SetSeed(102);
-        a2=rgg.TreeGraph(typeof(AdjacencyListsGraph<AVLAdjacencyList>),15,1);
-        a2.DelEdge(1,7);
-        a2.DelEdge(6,12);
-//        ge.Export(a2,"a2");
-        b2=a2.IsUndirectedAcyclic();
-        Console.WriteLine("Czy graf a2 jest acykliczny ? : {0} (powinno byc True)",b2);
+        a2 = rgg.TreeGraph(typeof(AdjacencyListsGraph<AVLAdjacencyList>), 15, 1);
+        a2.DelEdge(1, 7);
+        a2.DelEdge(6, 12);
+        //        ge.Export(a2,"a2");
+        b2 = a2.IsUndirectedAcyclic();
+        Console.WriteLine("Czy graf a2 jest acykliczny ? : {0} (powinno byc True)", b2);
 
         rgg.SetSeed(103);
-        a3=rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph),10,0.3);
-//        ge.Export(a3,"a3");
-        b3=a3.IsUndirectedAcyclic();
-        Console.WriteLine("Czy graf a3 jest acykliczny ? : {0} (powinno byc False)",b3);
+        a3 = rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph), 10, 0.3);
+        //        ge.Export(a3,"a3");
+        b3 = a3.IsUndirectedAcyclic();
+        Console.WriteLine("Czy graf a3 jest acykliczny ? : {0} (powinno byc False)", b3);
 
         rgg.SetSeed(104);
-        a4=rgg.DirectedGraph(typeof(AdjacencyMatrixGraph),1000,0.1);
+        a4 = rgg.DirectedGraph(typeof(AdjacencyMatrixGraph), 1000, 0.1);
         try
-            {
-            b4=a4.IsUndirectedAcyclic();
+        {
+            b4 = a4.IsUndirectedAcyclic();
             Console.WriteLine("Blad - powinien byc wyjatek");
-            }
-        catch ( ArgumentException e)
-            {
+        }
+        catch (ArgumentException e)
+        {
             Console.WriteLine(e.Message);
-            }
+        }
         Console.WriteLine("Czy graf a4 jest acykliczny ? (przed chwila powinien byc wyjatek)");
 
         rgg.SetSeed(105);
-        a5=rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph),2000,0.8);
-        b5=a5.IsUndirectedAcyclic();
-        Console.WriteLine("Czy graf a5 jest acykliczny ? : {0} (powinno byc False)",b5);
+        a5 = rgg.UndirectedGraph(typeof(AdjacencyMatrixGraph), 2000, 0.8);
+        b5 = a5.IsUndirectedAcyclic();
+        Console.WriteLine("Czy graf a5 jest acykliczny ? : {0} (powinno byc False)", b5);
 
         Console.WriteLine("KONIEC !!!\n");
-        }
-
     }
+
+}
