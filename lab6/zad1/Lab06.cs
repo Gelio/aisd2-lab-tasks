584d7b5af1ef5d6d119ad06097908c5c44faec03
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-03-31 17:03:23 +0200

    lab6/zad1: complete finding the biggest clique in a graph

--- lab6/zad1/Lab06.cs
+++ lab6/zad1/Lab06.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ASD.Graphs
 {
@@ -29,8 +30,81 @@ namespace ASD.Graphs
             // 4) zdefiniować pomocniczą funkcję rekurencyjną
             // 5) do badania krawędzi pomiędzy wierzchołkami i oraz j użyć metody GetEdgeWeight(i,j)
 
-            clique = new int[] { 0 };
-            return 1;
+            bool[] isVertexInClique = new bool[g.VerticesCount];
+            int cliqueNumber = CliqueNumberRecursive(g, ref isVertexInClique, 0);
+
+            List<int> cliqueList = new List<int>(g.VerticesCount);
+            for (int v=0; v < g.VerticesCount; v++)
+            {
+                if (isVertexInClique[v])
+                    cliqueList.Add(v);
+            }
+            clique = cliqueList.ToArray();
+            return cliqueNumber;
+        }
+
+        private static int CliqueNumberRecursive(Graph g, ref bool[] isVertexInClique, int cliqueSize, int startingVertex = 0)
+        {
+            bool[] biggestClique = isVertexInClique;
+            int biggestCliqueSize = cliqueSize;
+
+            if (cliqueSize > 0)
+            {
+                // Instead of analizying all the vertices
+                // we only take into account neighbors of the last vertex in the clique
+                // This is especially benefitial for sparse graphs (the last one in the test set)
+                foreach (Edge e in g.OutEdges(startingVertex - 1))
+                {
+                    if (e.To <= e.From)
+                        continue;
+
+                    CheckNewCliqueVertex(g, e.To, cliqueSize, ref isVertexInClique, ref biggestClique, ref biggestCliqueSize);
+                }
+                    
+            }
+            else
+            {
+                // In the initial step we analyze every vertex
+                // because any vertex is a valid clique
+                for (int v = startingVertex; v < g.VerticesCount; v++)
+                    CheckNewCliqueVertex(g, v, cliqueSize, ref isVertexInClique, ref biggestClique, ref biggestCliqueSize);
+            }
+
+            isVertexInClique = biggestClique;
+            return biggestCliqueSize;
+        }
+
+        // Helper method so as not to duplicate code in the CliqueNumberRecursive method
+        private static void CheckNewCliqueVertex(Graph g, int v, int cliqueSize, ref bool[] isVertexInClique, ref bool[] biggestClique, ref int biggestCliqueSize)
+        {
+            // If vertex has a lower degree than there are already vertices in the clique
+            // it definitely cannot be added to the clique
+            if (g.OutDegree(v) < cliqueSize || g.InDegree(v) < cliqueSize)
+                return;
+
+            // Check if there are edges between this vertex (v) and all the vertices already in the clique
+            bool validVertex = true;
+            for (int cliqueVertex = 0; cliqueVertex < v; cliqueVertex++)
+            {
+                if (isVertexInClique[cliqueVertex] && (g.GetEdgeWeight(v, cliqueVertex).IsNaN() || g.GetEdgeWeight(cliqueVertex, v).IsNaN()))
+                {
+                    validVertex = false;
+                    break;
+                }
+            }
+
+            if (!validVertex)
+                return;
+
+            isVertexInClique[v] = true;
+            bool[] newClique = isVertexInClique.Clone() as bool[];
+            int newCliqueSize = CliqueNumberRecursive(g, ref newClique, cliqueSize + 1, v + 1);
+            if (newCliqueSize > biggestCliqueSize)
+            {
+                biggestClique = newClique;
+                biggestCliqueSize = newCliqueSize;
+            }
+            isVertexInClique[v] = false;
         }
 
         /// <summary>
