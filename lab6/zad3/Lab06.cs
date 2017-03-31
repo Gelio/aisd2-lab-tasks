c32c13235e11848e29e5c9d5c90e6dde2d85fbc8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-03-31 17:58:58 +0200

    lab6/zad3: create project, add basic algorithm (WIP)

--- lab6/zad3/Lab06.cs
+++ lab6/zad3/Lab06.cs
@@ -13,7 +13,48 @@
         /// <returns>Wysokość optymalnego stosu</returns>
         public static int HighestStack(IList<Book> books, out IList<Book> stack)
         {
-            throw new NotImplementedException();
+            Stack<Book> highestStack = new Stack<Book>();
+            int highestStackHeight = 0;
+
+            HighestStackHelper(books, ref highestStack, ref highestStackHeight);
+
+            stack = new List<Book>(highestStack);
+            return highestStackHeight;
+        }
+
+        private static void HighestStackHelper(IList<Book> books, ref Stack<Book> currentStack, ref int currentStackHeight)
+        {
+            Stack<Book> highestStack = currentStack;
+            int highestStackHeight = currentStackHeight;
+            List<Book> currentStackReversed = new List<Book>(currentStack);
+            currentStackReversed.Reverse();
+
+            Book topBook = currentStack.Count > 0 ? currentStack.Peek() : null;
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (currentStack.Contains(books[i]))
+                    continue;
+
+                Book book = books[i];
+                if (topBook != null)
+                {
+                    if ((topBook.Height < book.Height || topBook.Width < book.Width) && (topBook.Height < book.Width || topBook.Width < book.Height))
+                        continue;
+                }
+
+                Stack<Book> newStack = new Stack<Book>(currentStackReversed);
+                newStack.Push(book);
+                int newStackHeight = currentStackHeight + book.Thickness;
+                HighestStackHelper(books, ref newStack, ref newStackHeight);
+                if (newStackHeight > highestStackHeight)
+                {
+                    highestStack = newStack;
+                    highestStackHeight = newStackHeight;
+                }
+            }
+
+            currentStack = highestStack;
+            currentStackHeight = highestStackHeight;
         }
     }
 }
--- lab6/zad3/books.cs
+++ lab6/zad3/books.cs
@@ -58,73 +58,73 @@
                 {
                     new TestSet()
                         {
-                            Name = "1", 
+                            Name = "1",
                             Books =
                                 new List<Book>()
                                     {
-                                        new Book(7, 6, 1, 6), 
-                                        new Book(1, 5, 6, 3), 
-                                        new Book(2, 4, 7, 2), 
-                                        new Book(3, 5, 9, 1), 
-                                        new Book(10, 11, 1, 9), 
-                                        new Book(11, 100, 1, 5), 
-                                        new Book(4, 3, 6, 2), 
-                                        new Book(5, 10, 9, 3), 
-                                        new Book(6, 7, 3, 5), 
-                                        new Book(8, 4, 8, 2), 
-                                        new Book(9, 6, 8, 3), 
-                                    }, 
-                            Height = 14, 
-                            StackIds = new List<int>() { 5, 6, 7 }, 
+                                        new Book(7, 6, 1, 6),
+                                        new Book(1, 5, 6, 3),
+                                        new Book(2, 4, 7, 2),
+                                        new Book(3, 5, 9, 1),
+                                        new Book(10, 11, 1, 9),
+                                        new Book(11, 100, 1, 5),
+                                        new Book(4, 3, 6, 2),
+                                        new Book(5, 10, 9, 3),
+                                        new Book(6, 7, 3, 5),
+                                        new Book(8, 4, 8, 2),
+                                        new Book(9, 6, 8, 3),
+                                    },
+                            Height = 14,
+                            StackIds = new List<int>() { 5, 6, 7 },
                             Efficiency = 400
-                        }, 
+                        },
 
                     new TestSet()
                         {
-                            Name = "2", 
+                            Name = "2",
                             Books =
                                 new List<Book>()
                                     {
-                                        new Book(7, 1, 1, 6), 
-                                        new Book(1, 2, 2, 3), 
-                                        new Book(2, 3, 3, 2), 
-                                        new Book(3, 4, 4, 1), 
-                                        new Book(10, 5, 5, 9), 
-                                        new Book(11, 6, 6, 5), 
-                                        new Book(4, 7, 7, 2), 
-                                        new Book(5, 8, 8, 3), 
-                                        new Book(6, 9, 9, 5), 
-                                        new Book(8, 10, 10, 2), 
-                                        new Book(9, 11, 11, 3), 
-                                        new Book(12, 12, 12, 3), 
-                                        new Book(13, 13, 13, 3), 
-                                        new Book(14, 14, 14, 3), 
-                                        new Book(15, 15, 15, 3), 
-                                        new Book(16, 16, 16, 3), 
-                                    }, 
-                            Height = 56, 
-                            StackIds = new List<int>() { 16, 15, 14, 13, 12, 9, 8, 6, 5, 4, 11, 10, 3, 2, 1, 7 }, 
+                                        new Book(7, 1, 1, 6),
+                                        new Book(1, 2, 2, 3),
+                                        new Book(2, 3, 3, 2),
+                                        new Book(3, 4, 4, 1),
+                                        new Book(10, 5, 5, 9),
+                                        new Book(11, 6, 6, 5),
+                                        new Book(4, 7, 7, 2),
+                                        new Book(5, 8, 8, 3),
+                                        new Book(6, 9, 9, 5),
+                                        new Book(8, 10, 10, 2),
+                                        new Book(9, 11, 11, 3),
+                                        new Book(12, 12, 12, 3),
+                                        new Book(13, 13, 13, 3),
+                                        new Book(14, 14, 14, 3),
+                                        new Book(15, 15, 15, 3),
+                                        new Book(16, 16, 16, 3),
+                                    },
+                            Height = 56,
+                            StackIds = new List<int>() { 16, 15, 14, 13, 12, 9, 8, 6, 5, 4, 11, 10, 3, 2, 1, 7 },
                             Efficiency = 1000
-                        }, 
+                        },
 
                     new TestSet()
                         {
-                            Name = "3", 
+                            Name = "3",
                             Books =
                                 new List<Book>()
                                     {
-                                        new Book(1, 10, 1, 15), 
-                                        new Book(2, 9, 8, 2), 
-                                        new Book(3, 8, 3, 10), 
-                                        new Book(4, 7, 4, 1), 
-                                        new Book(5, 8, 8, 3), 
-                                        new Book(6, 9, 9, 5), 
-                                        new Book(7, 5, 5, 9), 
-                                        new Book(8, 6, 6, 5), 
-                                        new Book(9, 7, 7, 2), 
-                                    }, 
-                            Height = 24, 
-                            StackIds = new List<int>() { 6, 5, 9, 8, 7 }, 
+                                        new Book(1, 10, 1, 15),
+                                        new Book(2, 9, 8, 2),
+                                        new Book(3, 8, 3, 10),
+                                        new Book(4, 7, 4, 1),
+                                        new Book(5, 8, 8, 3),
+                                        new Book(6, 9, 9, 5),
+                                        new Book(7, 5, 5, 9),
+                                        new Book(8, 6, 6, 5),
+                                        new Book(9, 7, 7, 2),
+                                    },
+                            Height = 24,
+                            StackIds = new List<int>() { 6, 5, 9, 8, 7 },
                             Efficiency = 300
                         }
                 };
