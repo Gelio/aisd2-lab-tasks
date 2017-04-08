f63c9efb947a0c191ba940f589efc5f6c647c7df
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-08 13:46:10 +0200

    lab8/zad1: create project, adjust code to the latest graph library version

--- lab8/zad1/Lab08.cs
+++ lab8/zad1/Lab08.cs
@@ -1,63 +1,63 @@
 
-namespace ASD.Graph
+namespace ASD.Graphs
 {
-using System.Linq;  // potrzebne dla metody First rozszerzającej interfejs IEnumerable
+    using System.Linq;  // potrzebne dla metody First rozszerzającej interfejs IEnumerable
 
-/// <summary>
-/// Rozszerzenie interfejsu <see cref="IGraph"/> o rozwiązywanie problemu komiwojażera metodami przybliżonymi
-/// </summary>
-public static class Lab08Extender
+    /// <summary>
+    /// Rozszerzenie interfejsu <see cref="IGraph"/> o rozwiązywanie problemu komiwojażera metodami przybliżonymi
+    /// </summary>
+    public static class Lab08Extender
     {
 
-    /// <summary>
-    /// Znajduje rozwiązanie przybliżone problemu komiwojażera algorytmem zachłannym "kruskalopodobnym"
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <param name="cycle">Znaleziony cykl (parametr wyjściowy)</param>
-    /// <returns>Długość znalezionego cyklu (suma wag krawędzi)</returns>
-    /// <remarks>
-    /// Elementy (krawędzie) umieszczone są w tablicy <i>cycle</i> w kolejności swojego następstwa w znalezionym cyklu Hamiltona.<br/>
-    /// <br/>
-    /// Jeśli algorytm "kruskalopodobny" nie znajdzie w badanym grafie cyklu Hamiltona
-    /// (co oczywiście nie znaczy, że taki cykl nie istnieje) to metoda zwraca <b>null</b>,
-    /// parametr wyjściowy <i>cycle</i> również ma wówczas wartość <b>null</b>.<br/>
-    /// <br/>
-    /// Metodę można stosować dla grafów skierowanych i nieskierowanych.<br/>
-    /// <br/>
-    /// Metodę można stosować dla dla grafów z dowolnymi (również ujemnymi) wagami krawędzi.
-    /// </remarks>
-    public static int? TSP_Kruskal(this IGraph g, out Edge[] cycle)
+        /// <summary>
+        /// Znajduje rozwiązanie przybliżone problemu komiwojażera algorytmem zachłannym "kruskalopodobnym"
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="cycle">Znaleziony cykl (parametr wyjściowy)</param>
+        /// <returns>Długość znalezionego cyklu (suma wag krawędzi)</returns>
+        /// <remarks>
+        /// Elementy (krawędzie) umieszczone są w tablicy <i>cycle</i> w kolejności swojego następstwa w znalezionym cyklu Hamiltona.<br/>
+        /// <br/>
+        /// Jeśli algorytm "kruskalopodobny" nie znajdzie w badanym grafie cyklu Hamiltona
+        /// (co oczywiście nie znaczy, że taki cykl nie istnieje) to metoda zwraca <b>null</b>,
+        /// parametr wyjściowy <i>cycle</i> również ma wówczas wartość <b>null</b>.<br/>
+        /// <br/>
+        /// Metodę można stosować dla grafów skierowanych i nieskierowanych.<br/>
+        /// <br/>
+        /// Metodę można stosować dla dla grafów z dowolnymi (również ujemnymi) wagami krawędzi.
+        /// </remarks>
+        public static int? TSP_Kruskal(this Graph g, out Edge[] cycle)
         {
-        // ToDo - algorytm "kruskalopodobny"
-        cycle=null;
-        return null;
+            // ToDo - algorytm "kruskalopodobny"
+            cycle = null;
+            return null;
         }  // TSP_Kruskal
 
-    /// <summary>
-    /// Znajduje rozwiązanie przybliżone problemu komiwojażera tworząc cykl Hamiltona na podstawie drzewa rozpinającego
-    /// </summary>
-    /// <param name="g">Badany graf</param>
-    /// <param name="cycle">Znaleziony cykl (parametr wyjściowy)</param>
-    /// <returns>Długość znalezionego cyklu (suma wag krawędzi)</returns>
-    /// <remarks>
-    /// Elementy (krawędzie) umieszczone są w tablicy <i>cycle</i> w kolejności swojego następstwa w znalezionym cyklu Hamiltona.<br/>
-    /// <br/>
-    /// Jeśli algorytm bazujący na drzewie rozpinającym nie znajdzie w badanym grafie cyklu Hamiltona
-    /// (co oczywiście nie znaczy, że taki cykl nie istnieje) to metoda zwraca <b>null</b>,
-    /// parametr wyjściowy <i>cycle</i> również ma wówczas wartość <b>null</b>.<br/>
-    /// <br/>
-    /// Metodę można stosować dla grafów nieskierowanych.<br/>
-    /// Zastosowana do grafu skierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.<br/>
-    /// <br/>
-    /// Metodę można stosować dla dla grafów z dowolnymi (również ujemnymi) wagami krawędzi.<br/>
-    /// <br/>
-    /// Dla grafu nieskierowanego spełniajacego nierówność trójkąta metoda realizuje algorytm 2-aproksymacyjny.
-    /// </remarks>
-    public static int? TSP_TreeBased(this IGraph g, out Edge[] cycle)
+        /// <summary>
+        /// Znajduje rozwiązanie przybliżone problemu komiwojażera tworząc cykl Hamiltona na podstawie drzewa rozpinającego
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="cycle">Znaleziony cykl (parametr wyjściowy)</param>
+        /// <returns>Długość znalezionego cyklu (suma wag krawędzi)</returns>
+        /// <remarks>
+        /// Elementy (krawędzie) umieszczone są w tablicy <i>cycle</i> w kolejności swojego następstwa w znalezionym cyklu Hamiltona.<br/>
+        /// <br/>
+        /// Jeśli algorytm bazujący na drzewie rozpinającym nie znajdzie w badanym grafie cyklu Hamiltona
+        /// (co oczywiście nie znaczy, że taki cykl nie istnieje) to metoda zwraca <b>null</b>,
+        /// parametr wyjściowy <i>cycle</i> również ma wówczas wartość <b>null</b>.<br/>
+        /// <br/>
+        /// Metodę można stosować dla grafów nieskierowanych.<br/>
+        /// Zastosowana do grafu skierowanego zgłasza wyjątek <see cref="System.ArgumentException"/>.<br/>
+        /// <br/>
+        /// Metodę można stosować dla dla grafów z dowolnymi (również ujemnymi) wagami krawędzi.<br/>
+        /// <br/>
+        /// Dla grafu nieskierowanego spełniajacego nierówność trójkąta metoda realizuje algorytm 2-aproksymacyjny.
+        /// </remarks>
+        public static int? TSP_TreeBased(this Graph g, out Edge[] cycle)
         {
-        // ToDo - algorytm oparty na minimalnym drzewie rozpinajacym
-        cycle=null;
-        return null;
+            // ToDo - algorytm oparty na minimalnym drzewie rozpinajacym
+            cycle = null;
+            return null;
         }  // TSP_TreeBased
 
     }  // class Lab08Extender
--- lab8/zad1/Lab08_main.cs
+++ lab8/zad1/Lab08_main.cs
@@ -1,49 +1,49 @@
 
 using System;
-using ASD.Graph;
+using ASD.Graphs;
 
 class Lab08
+{
+
+    public static bool HamiltonTest(Graph g, Edge[] cycle, int len)
     {
-
-    public static bool HamiltonTest(IGraph g, Edge[] cycle, int len)
-        {
-        if ( g.VerticesCount!=cycle.Length ) return false;
-        int l;
+        if (g.VerticesCount != cycle.Length) return false;
+        double l;
         bool[] v = new bool[g.VerticesCount];
-        l=cycle[0].Weight;
-        v[cycle[0].From]=true;
-        for ( int i=1 ; i<g.VerticesCount ; ++i )
-            {
-            if ( cycle[i].From!=cycle[i-1].To ) return false;
-            if ( v[cycle[i].From] ) return false;
-            v[cycle[i].From]=true;
-            l+=cycle[i].Weight;
-            }
-        return ( cycle[0].From==cycle[g.VerticesCount-1].To ) && l==len;
-        }
-
-    public static void Test(IGraph g, Edge[] c, int? m)
+        l = cycle[0].Weight;
+        v[cycle[0].From] = true;
+        for (int i = 1; i < g.VerticesCount; ++i)
         {
-        if ( m==null && c==null )
-            {
+            if (cycle[i].From != cycle[i - 1].To) return false;
+            if (v[cycle[i].From]) return false;
+            v[cycle[i].From] = true;
+            l += cycle[i].Weight;
+        }
+        return (cycle[0].From == cycle[g.VerticesCount - 1].To) && l == len;
+    }
+
+    public static void Test(Graph g, Edge[] c, int? m)
+    {
+        if (m == null && c == null)
+        {
             Console.WriteLine("nie znaleziono cyklu Hamiltona");
             return;
-            }
-        if ( m==null || c==null )
-            {
+        }
+        if (m == null || c == null)
+        {
             Console.WriteLine("BLAD 1  !!!");
             return;
-            }
-        if ( HamiltonTest(g,c,(int)m) )
-            Console.WriteLine("znaleziono cykl Hamiltona dlugosci {0}",m);
+        }
+        if (HamiltonTest(g, c, (int)m))
+            Console.WriteLine("znaleziono cykl Hamiltona dlugosci {0}", m);
         else
             Console.WriteLine("BLAD 2  !!!");
-        }
+    }
 
-    public static void Main ()
-        {
+    public static void Main()
+    {
         int? m;
-        IGraph g0, g1, g2, g3, g4, g5, g6, g7;
+        Graph g0, g1, g2, g3, g4, g5, g6, g7;
         Edge[] c;
 
         RandomGraphGenerator gen = new RandomGraphGenerator(1);
@@ -53,115 +53,115 @@ class Lab08
         g3 = gen.DirectedGraph(typeof(AdjacencyMatrixGraph), 100, 1.0, 1, 99);
         g4 = gen.UndirectedGraph(typeof(AdjacencyMatrixGraph), 100, 0.9, 1, 99);
         g5 = gen.DirectedGraph(typeof(AdjacencyMatrixGraph), 100, 0.9, 1, 99);
-        g6 = gen.UndirectedGraph(typeof(AdjacencyListsGraph), 100, 0.2, 1, 99);
-        g7 = gen.DirectedGraph(typeof(AdjacencyListsGraph), 100, 0.2, 1, 99);
+        g6 = gen.UndirectedGraph(typeof(AdjacencyListsGraph<AVLAdjacencyList>), 100, 0.2, 1, 99);
+        g7 = gen.DirectedGraph(typeof(AdjacencyListsGraph<AVLAdjacencyList>), 100, 0.2, 1, 99);
 
         Console.WriteLine("\nAlgorytm \"Kruskalopodobny\"");
 
         Console.Write("  maly graf euklidesowy     -  ");
-        m=g0.TSP_Kruskal(out c);
-        Test(g0,c,m);
-        if ( m==null )
+        m = g0.TSP_Kruskal(out c);
+        Test(g0, c, m);
+        if (m == null)
             Console.WriteLine("    Nie znaleziono cyklu Hamiltona");
         else
-            {
+        {
             Console.Write("    ");
-            for ( int i=0 ; i<g0.VerticesCount ; ++ i )
-                Console.Write("  {0}",c[i]);
+            for (int i = 0; i < g0.VerticesCount; ++i)
+                Console.Write("  {0}", c[i]);
             Console.WriteLine();
-            }
+        }
 
         Console.Write("  graf pelny euklidesowy    -  ");
-        m=g1.TSP_Kruskal(out c);
-        Test(g1,c,m);
+        m = g1.TSP_Kruskal(out c);
+        Test(g1, c, m);
 
         Console.Write("  graf pelny nieskierowany  -  ");
-        m=g2.TSP_Kruskal(out c);
-        Test(g2,c,m);
+        m = g2.TSP_Kruskal(out c);
+        Test(g2, c, m);
 
         Console.Write("  graf pelny skierowany     -  ");
-        m=g3.TSP_Kruskal(out c);
-        Test(g3,c,m);
+        m = g3.TSP_Kruskal(out c);
+        Test(g3, c, m);
 
         Console.Write("  graf nieskierowany        -  ");
-        m=g4.TSP_Kruskal(out c);
-        Test(g4,c,m);
+        m = g4.TSP_Kruskal(out c);
+        Test(g4, c, m);
 
         Console.Write("  graf skierowany           -  ");
-        m=g5.TSP_Kruskal(out c);
-        Test(g5,c,m);
+        m = g5.TSP_Kruskal(out c);
+        Test(g5, c, m);
 
         Console.Write("  graf rzadki nieskierowany -  ");
-        m=g6.TSP_Kruskal(out c);
-        Test(g6,c,m);
+        m = g6.TSP_Kruskal(out c);
+        Test(g6, c, m);
 
         Console.Write("  graf rzadki skierowany    -  ");
-        m=g7.TSP_Kruskal(out c);
-        Test(g7,c,m);
+        m = g7.TSP_Kruskal(out c);
+        Test(g7, c, m);
 
         Console.WriteLine("\nAlgorytm na podstawie drzewa");
 
         Console.Write("  maly graf euklidesowy     -  ");
-        m=g0.TSP_TreeBased(out c);
-        Test(g0,c,m);
-        if ( m==null )
+        m = g0.TSP_TreeBased(out c);
+        Test(g0, c, m);
+        if (m == null)
             Console.WriteLine("    Nie znaleziono cyklu Hamiltona");
         else
-            {
+        {
             Console.Write("    ");
-            for ( int i=0 ; i<g0.VerticesCount ; ++ i )
-                Console.Write("  {0}",c[i]);
+            for (int i = 0; i < g0.VerticesCount; ++i)
+                Console.Write("  {0}", c[i]);
             Console.WriteLine();
-            }
+        }
 
         Console.Write("  graf pelny euklidesowy    -  ");
-        m=g1.TSP_TreeBased(out c);
-        Test(g1,c,m);
+        m = g1.TSP_TreeBased(out c);
+        Test(g1, c, m);
 
         Console.Write("  graf pelny nieskierowany  -  ");
-        m=g2.TSP_TreeBased(out c);
-        Test(g2,c,m);
+        m = g2.TSP_TreeBased(out c);
+        Test(g2, c, m);
 
         Console.Write("  graf pelny skierowany     -  ");
         try
-            {
-            m=g3.TSP_TreeBased(out c);
+        {
+            m = g3.TSP_TreeBased(out c);
             Console.WriteLine("BLAD 3  !!!");
-            }
-        catch ( ArgumentException )
-            {
+        }
+        catch (ArgumentException)
+        {
             Console.WriteLine("wyjatek (to dobrze)");
-            }
+        }
 
         Console.Write("  graf nieskierowany        -  ");
-        m=g4.TSP_TreeBased(out c);
-        Test(g4,c,m);
+        m = g4.TSP_TreeBased(out c);
+        Test(g4, c, m);
 
         Console.Write("  graf skierowany           -  ");
         try
-            {
-            m=g5.TSP_TreeBased(out c);
+        {
+            m = g5.TSP_TreeBased(out c);
             Console.WriteLine("BLAD 3  !!!");
-            }
-        catch ( ArgumentException )
-            {
+        }
+        catch (ArgumentException)
+        {
             Console.WriteLine("wyjatek (to dobrze)");
-            }
+        }
 
         Console.Write("  graf rzadki nieskierowany -  ");
-        m=g6.TSP_TreeBased(out c);
-        Test(g6,c,m);
+        m = g6.TSP_TreeBased(out c);
+        Test(g6, c, m);
 
         Console.Write("  graf rzadki skierowany    -  ");
         try
-            {
-            m=g7.TSP_TreeBased(out c);
+        {
+            m = g7.TSP_TreeBased(out c);
             Console.WriteLine("BLAD 3  !!!");
-            }
-        catch ( ArgumentException )
-            {
-            Console.WriteLine("wyjatek (to dobrze)");
-            }
         }
-
+        catch (ArgumentException)
+        {
+            Console.WriteLine("wyjatek (to dobrze)");
+        }
     }
+
+}
