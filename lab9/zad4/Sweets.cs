0f62fb186479f46258ccee8126c78c8030957abe
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-16 22:49:05 +0200

    lab9/zad4: complete task 2a

--- lab9/zad4/Sweets.cs
+++ lab9/zad4/Sweets.cs
@@ -29,7 +29,7 @@ namespace Lab09
             // (2 + childrenCount), ..., (2 + childrenCount + sweetsCount - 1) - sweets
 
             // Each child can have only one sweet
-            for (int i=0; i < childrenCount; i++)
+            for (int i = 0; i < childrenCount; i++)
                 sweetsDispenser.AddEdge(0, 2 + i, 1);
 
             // There is only one piece of each sweet
@@ -37,7 +37,7 @@ namespace Lab09
                 sweetsDispenser.AddEdge(2 + childrenCount + i, 1, 1);
 
             // Children' likes
-            for (int i=0; i < childrenCount; i++)
+            for (int i = 0; i < childrenCount; i++)
             {
                 foreach (int sweet in childrenLikes[i])
                     sweetsDispenser.AddEdge(2 + i, 2 + childrenCount + sweet, 1);
@@ -46,7 +46,7 @@ namespace Lab09
             int childrenSatisfied = (int)sweetsDispenser.FordFulkersonDinicMaxFlow(0, 1, out Graph sweetsAssignment, MaxFlowGraphExtender.BFPath);
 
             assignment = new int[childrenCount];
-            for (int i=0; i < childrenCount; i++)
+            for (int i = 0; i < childrenCount; i++)
             {
                 assignment[i] = -1;
                 foreach (Edge e in sweetsAssignment.OutEdges(2 + i))
@@ -54,7 +54,7 @@ namespace Lab09
                     if (e.Weight > 0)
                         assignment[i] = e.To - (2 + childrenCount);
                 }
-                    
+
             }
             return childrenSatisfied;
         }
@@ -75,9 +75,43 @@ namespace Lab09
         /// <returns>Maksymalna liczba rozdanych batoników.</returns>
         public static int Task2(int childrenCount, int sweetsCount, int[][] childrenLikes, int[] childrenLimits, int[] sweetsLimits, out bool[] happyChildren, out int[] shoppingList)
         {
-            happyChildren = null;
+            Graph sweetsDispenser = new AdjacencyMatrixGraph(true, childrenCount + sweetsCount + 2);
+            // Vertices in the graph
+            // 0 - source, 1 - destination,
+            // 2, ..., (2 + childrenCount - 1) - children
+            // (2 + childrenCount), ..., (2 + childrenCount + sweetsCount - 1) - sweets
+
+            // Each child has a limit on the amount of sweets it can it
+            for (int i = 0; i < childrenCount; i++)
+                sweetsDispenser.AddEdge(0, 2 + i, childrenLimits[i]);
+
+            // Limited number of each sweet
+            for (int i = 0; i < sweetsCount; i++)
+                sweetsDispenser.AddEdge(2 + childrenCount + i, 1, sweetsLimits[i]);
+
+            // Children' likes
+            for (int i = 0; i < childrenCount; i++)
+            {
+                foreach (int sweet in childrenLikes[i])
+                    sweetsDispenser.AddEdge(2 + i, 2 + childrenCount + sweet, double.PositiveInfinity);
+            }
+
+            int totalSweetsTaken = (int)sweetsDispenser.FordFulkersonDinicMaxFlow(0, 1, out Graph sweetsAssignment, MaxFlowGraphExtender.BFPath);
+
+            happyChildren = new bool[childrenCount];
+            for (int i = 0; i < childrenCount; i++)
+            {
+                double childSum = 0;
+                foreach (Edge e in sweetsAssignment.OutEdges(2 + i))
+                {
+                    childSum += e.Weight;
+                }
+                happyChildren[i] = (int)childSum == childrenLimits[i];
+            }
+
+
             shoppingList = null;
-            return -1;
+            return totalSweetsTaken;
         }
 
     }
