5530cbd6b3ac155b040752e23f6630e7c7947966
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-29 15:06:20 +0200

    lab9/zad3: adjust code to the newest library version

--- lab9/zad3/Coloring.cs
+++ lab9/zad3/Coloring.cs
@@ -1,67 +1,77 @@
 
-namespace ASD.Graph
+namespace ASD.Graphs
 {
 
-public static class ColoringExtender
+    public static class ColoringExtender
     {
 
-    // koloruje graf algorytmem zachlannym (byc moze niepotymalnie)
-    public static int GreedyColor(this IGraph g, out int[] colors)
+        // koloruje graf algorytmem zachlannym (byc moze niepotymalnie)
+        public static int GreedyColor(this Graph g, out int[] colors)
         {
-        // kazdemu wierzcholkowi 
-        // przydzielamy najmniejszy kolor nie kolidujacy z juz pokolorowanymi sasiadami
-        // (wpisujemy go do tablicy colors)
-        // zwracamy liczbe uzytych kolorow
+            // kazdemu wierzcholkowi 
+            // przydzielamy najmniejszy kolor nie kolidujacy z juz pokolorowanymi sasiadami
+            // (wpisujemy go do tablicy colors)
+            // zwracamy liczbe uzytych kolorow
 
-        /* ZMIENIC */ int k=g.VerticesCount;
-        /* ZMIENIC */ colors=new int[k];
-        /* ZMIENIC */ for ( int i=0 ; i<k ; ++i )
-        /* ZMIENIC */     colors[i]=i+1;
-        /* ZMIENIC */ return k;
+            /* ZMIENIC */
+            int k = g.VerticesCount;
+            /* ZMIENIC */
+            colors = new int[k];
+            /* ZMIENIC */
+            for (int i = 0; i < k; ++i)
+                /* ZMIENIC */
+                colors[i] = i + 1;
+            /* ZMIENIC */
+            return k;
         }
 
-    // koloruje graf algorytmem z powrotami (optymalnie)
-    public static int BacktrackingColor(this IGraph g, out int[] colors)
+        // koloruje graf algorytmem z powrotami (optymalnie)
+        public static int BacktrackingColor(this Graph g, out int[] colors)
         {
-        var gc = new Coloring(g);
-        gc.Color(0,new int[g.VerticesCount],0);
-        colors=gc.bestColors;
-        return gc.bestColorsNumber;
+            var gc = new Coloring(g);
+            gc.Color(0, new int[g.VerticesCount], 0);
+            colors = gc.bestColors;
+            return gc.bestColorsNumber;
         }
 
-    // klasa pomocnicza dla algorytmu z powrotami
-    private sealed class Coloring
+        // klasa pomocnicza dla algorytmu z powrotami
+        private sealed class Coloring
         {
-        
-        // tablica pamietajaca najlepsze dotychczas znalezione pokolorowanie
-        internal int[] bestColors=null;
 
-        // zmienna pamietajaca liczbe kolorow w najlepszym dotychczas znalezionym pokolorowaniu
-        internal int bestColorsNumber;
+            // tablica pamietajaca najlepsze dotychczas znalezione pokolorowanie
+            internal int[] bestColors = null;
 
-        // badany graf
-        private IGraph g;
-        
-        // konstruktor
-        internal Coloring(IGraph g)
+            // zmienna pamietajaca liczbe kolorow w najlepszym dotychczas znalezionym pokolorowaniu
+            internal int bestColorsNumber;
+
+            // badany graf
+            private Graph g;
+
+            // konstruktor
+            internal Coloring(Graph g)
             {
-            this.g=g;
-            bestColorsNumber=g.VerticesCount+1;
+                this.g = g;
+                bestColorsNumber = g.VerticesCount + 1;
             }
 
-        // rekurencyjna metoda znajdujaca najlepsze pokolorowanie
-        // v - wierzcholek do pokolorowania
-        // colors - tablica kolorow
-        // n - liczba kolorow uzytych w pokolorowaniu zapisanym w colors
-        internal void Color(int v ,int[] colors, int n)
+            // rekurencyjna metoda znajdujaca najlepsze pokolorowanie
+            // v - wierzcholek do pokolorowania
+            // colors - tablica kolorow
+            // n - liczba kolorow uzytych w pokolorowaniu zapisanym w colors
+            internal void Color(int v, int[] colors, int n)
             {
-            // tu zaimplementowac algorytm z powrotami
+                // tu zaimplementowac algorytm z powrotami
 
-            /* ZMIENIC */ int k=g.VerticesCount;
-            /* ZMIENIC */ bestColors=new int[k];
-            /* ZMIENIC */ for ( int i=0 ; i<k ; ++i )
-            /* ZMIENIC */     bestColors[i]=i+1;
-            /* ZMIENIC */ bestColorsNumber=k;
+                /* ZMIENIC */
+                int k = g.VerticesCount;
+                /* ZMIENIC */
+                bestColors = new int[k];
+                /* ZMIENIC */
+                for (int i = 0; i < k; ++i)
+                    /* ZMIENIC */
+                    bestColors[i] = i + 1;
+                /* ZMIENIC */
+                bestColorsNumber = k;
             }
 
         }  // class Coloring
