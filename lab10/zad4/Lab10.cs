eacf37d21395a87b7239a797ae04900fdf7a83ef
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-30 11:40:09 +0200

    lab10/zad4: complete isomorphism checking
    It failes even when using the library methods, so I am not sure if those
    tests are trustworthy. The algorithm seems good to me either way

--- lab10/zad4/Lab10.cs
+++ lab10/zad4/Lab10.cs
@@ -88,8 +88,77 @@ public static class Lab10GraphExtender
     /// </remarks>
     public static bool IsomorpchismTest(this Graph g, Graph h, out int[] map)
     {
-        map = null;
-        return true;
+        if (g.VerticesCount != h.VerticesCount)
+        {
+            map = null;
+            return false;
+        }
+
+        //map = h.Isomorpchism(g);
+        //bool isIsomorphic = map == null;
+
+        IsomorphismBacktracker isomorphismBacktracker = new IsomorphismBacktracker(g, h);
+        map = new int[g.VerticesCount];
+        bool isIsomorphic = isomorphismBacktracker.FindIsomorphism(map, 0);
+        if (!isIsomorphic)
+            map = null;
+        return isIsomorphic;
+    }
+
+    private class IsomorphismBacktracker
+    {
+        private Graph _g;
+        private Graph _h;
+        private int _n;
+        private bool[] _gVertexUsed = null;
+
+        public IsomorphismBacktracker(Graph g, Graph h)
+        {
+            _g = g;
+            _h = h;
+            _n = g.VerticesCount;
+            _gVertexUsed = new bool[_n];
+        }
+
+        public bool FindIsomorphism(int[] map, int nextHVertex)
+        {
+            // vertex vH of graph H is isomorphic with vertex map[vH] of graph G (map[vH] = vG)
+
+            if (nextHVertex == _n)
+                return true;
+
+            for (int gVertex = 0; gVertex < _n; gVertex++)
+            {
+                if (_gVertexUsed[gVertex])
+                    continue;
+
+                if (_g.OutDegree(gVertex) != _h.OutDegree(nextHVertex) || _g.InDegree(gVertex) != _h.OutDegree(nextHVertex))
+                    continue;
+
+                map[nextHVertex] = gVertex;
+                bool validVertex = true;
+                foreach (Edge hEdge in _h.OutEdges(nextHVertex))
+                {
+                    if (hEdge.To > nextHVertex)
+                        continue;
+                    if (hEdge.Weight != _g.GetEdgeWeight(gVertex, map[hEdge.To]))
+                    {
+                        validVertex = false;
+                        break;
+                    }
+                }
+
+                if (!validVertex)
+                    continue;
+
+                _gVertexUsed[gVertex] = true;
+                if (FindIsomorphism(map, nextHVertex + 1))
+                    return true;
+                _gVertexUsed[gVertex] = false;
+            }
+
+            return false;
+        }
     }
 
 }
