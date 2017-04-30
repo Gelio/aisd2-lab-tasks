9670b9aab6d3e422e8e8b8b116dba13cad6d18c1
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-30 11:18:22 +0200

    lab10/zad4: complete clique finding

--- lab10/zad4/Lab10.cs
+++ lab10/zad4/Lab10.cs
@@ -19,8 +19,59 @@ public static class Lab10GraphExtender
     /// </remarks>
     public static int MaxClique(this Graph g, out int[] clique)
     {
-        clique = null;
-        return 0;
+        CliqueBacktracker cliqueBacktracker = new CliqueBacktracker(g);
+        cliqueBacktracker.FindMaxClique(new List<int>(g.VerticesCount), 0);
+        clique = cliqueBacktracker.maxClique;
+        return clique.Length;
+    }
+
+    private class CliqueBacktracker
+    {
+        private Graph _g;
+        private int _n;
+        public int[] maxClique = null;
+        public int maxCliqueSize = 0;
+
+        public CliqueBacktracker(Graph g)
+        {
+            _g = g;
+            _n = g.VerticesCount;
+        }
+
+        public void FindMaxClique(List<int> currentClique, int nextV)
+        {
+            if (currentClique.Count > maxCliqueSize)
+            {
+                maxClique = currentClique.ToArray();
+                maxCliqueSize = currentClique.Count;
+            }
+
+            if (nextV == _n)
+                return;
+
+            if (currentClique.Count == _n || maxCliqueSize == _n)
+                return;
+
+            for (int v = nextV; v < _n; v++)
+            {
+                bool extendsClique = true;
+                foreach (int cliqueV in currentClique)
+                {
+                    if (_g.GetEdgeWeight(cliqueV, v).IsNaN() || _g.GetEdgeWeight(v, cliqueV).IsNaN())
+                    {
+                        extendsClique = false;
+                        break;
+                    }
+                }
+
+                if (!extendsClique)
+                    continue;
+
+                currentClique.Add(v);
+                FindMaxClique(currentClique, v + 1);
+                currentClique.RemoveAt(currentClique.Count - 1);
+            }
+        }
     }
 
     /// <summary>
