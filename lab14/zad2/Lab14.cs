9b705932f9430cc3b80940ed83012797e7c954ca
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-06-02 18:09:15 +0200

    lab14/zad2: create project, format code

--- lab14/zad2/Lab14.cs
+++ lab14/zad2/Lab14.cs
@@ -3,51 +3,51 @@ using ASD.Text;
 
 namespace ASD2
 {
-using System.Collections.Generic;
+    using System.Collections.Generic;
 
-public static partial class Lab14
+    public static partial class Lab14
     {
 
-    /// <summary>
-    /// Wyznaczanie minimalnego szablonu
-    /// </summary>
-    /// <param name="text">Napis do namalowania</param>
-    /// <param name="indexes">Tablica zawierająca indeksy "punktów przyłozenia" początku szablonu</param>
-    /// <returns>Długość szablonu</returns>
-    public static int MinPattern(string text, out int[] indexes)
+        /// <summary>
+        /// Wyznaczanie minimalnego szablonu
+        /// </summary>
+        /// <param name="text">Napis do namalowania</param>
+        /// <param name="indexes">Tablica zawierająca indeksy "punktów przyłozenia" początku szablonu</param>
+        /// <returns>Długość szablonu</returns>
+        public static int MinPattern(string text, out int[] indexes)
         {
-        indexes = new int[1] {0};
-        return text.Length;
+            indexes = new int[1] { 0 };
+            return text.Length;
         }
 
-    /// <summary>
-    /// Badanie równoważności cyklicznej z wykorzystaniem algorytmu KMP
-    /// </summary>
-    /// <param name="text1">Pierwszy badany tekst</param>
-    /// <param name="text2">Drugi badany tekst</param>
-    /// <returns>
-    /// Jeśli teksty nie są cyklicznie równoważne zwraca null
-    /// Jeśli teksty są cyklicznie równoważne zwraca liczbę znaków,
-    ///    które trzeba przenieść z początku pierwszego tekstu na koniec aby uzyskać drugi tekst
-    /// </returns>
-    public static int? CyclicEquivalenceKMP(string text1, string text2)
+        /// <summary>
+        /// Badanie równoważności cyklicznej z wykorzystaniem algorytmu KMP
+        /// </summary>
+        /// <param name="text1">Pierwszy badany tekst</param>
+        /// <param name="text2">Drugi badany tekst</param>
+        /// <returns>
+        /// Jeśli teksty nie są cyklicznie równoważne zwraca null
+        /// Jeśli teksty są cyklicznie równoważne zwraca liczbę znaków,
+        ///    które trzeba przenieść z początku pierwszego tekstu na koniec aby uzyskać drugi tekst
+        /// </returns>
+        public static int? CyclicEquivalenceKMP(string text1, string text2)
         {
-        return null;
+            return null;
         }
 
-    /// <summary>
-    /// Bezposrednie badanie równoważności cyklicznej
-    /// </summary>
-    /// <param name="text1">Pierwszy badany tekst</param>
-    /// <param name="text2">Drugi badany tekst</param>
-    /// <returns>
-    /// Jeśli teksty nie są cyklicznie równoważne zwraca null
-    /// Jeśli teksty są cyklicznie równoważne zwraca liczbę znaków,
-    ///    które trzeba przenieść z początku pierwszego tekstu na koniec aby uzyskać drugi tekst
-    /// </returns>
-    public static int? CyclicEquivalenceDirect(string text1, string text2)
+        /// <summary>
+        /// Bezposrednie badanie równoważności cyklicznej
+        /// </summary>
+        /// <param name="text1">Pierwszy badany tekst</param>
+        /// <param name="text2">Drugi badany tekst</param>
+        /// <returns>
+        /// Jeśli teksty nie są cyklicznie równoważne zwraca null
+        /// Jeśli teksty są cyklicznie równoważne zwraca liczbę znaków,
+        ///    które trzeba przenieść z początku pierwszego tekstu na koniec aby uzyskać drugi tekst
+        /// </returns>
+        public static int? CyclicEquivalenceDirect(string text1, string text2)
         {
-        return null;
+            return null;
         }
 
     }
--- lab14/zad2/Lab14_main.cs
+++ lab14/zad2/Lab14_main.cs
@@ -6,65 +6,65 @@ using ASD.Text;
 namespace ASD2
 {
 
-class TestP
+    class TestP
     {
-    public string text;
-    public int len;
-    public TestP(string t, int l)
+        public string text;
+        public int len;
+        public TestP(string t, int l)
         {
-        text=t;
-        len=l;
+            text = t;
+            len = l;
         }
     }
 
-class TestE
+    class TestE
     {
-    public string text1;
-    public string text2;
-    public int? ind;
-    public TestE(string t1, string t2, int? i)
+        public string text1;
+        public string text2;
+        public int? ind;
+        public TestE(string t1, string t2, int? i)
         {
-        text1=t1;
-        text2=t2;
-        ind=i;;
+            text1 = t1;
+            text2 = t2;
+            ind = i; ;
         }
     }
 
-public static partial class Lab14
+    public static partial class Lab14
     {
 
-    public static void Main()
+        public static void Main()
         {
-        TestP[] tests1 = {
+            TestP[] tests1 = {
             new TestP("aaaaa",1),
             new TestP("ababab",2),
             new TestP("abababa",3),
             new TestP("abcabcab",5),
             new TestP("ababbababbabababbabaababbababbaba",8),
             };
-        TestP[] tests1w = new TestP[2];
-        System.Text.StringBuilder sb = new System.Text.StringBuilder(100001);
-        for ( int i=0 ; i<100000 ; ++ i ) sb.Append("a");
-        tests1w[0] = new TestP(sb.ToString(),1);
-        tests1w[1] = new TestP(sb.ToString()+"b",100001);
-        int count;
-        int len;
-        int[] ind;
-        int? cei;
-        Console.WriteLine();
+            TestP[] tests1w = new TestP[2];
+            System.Text.StringBuilder sb = new System.Text.StringBuilder(100001);
+            for (int i = 0; i < 100000; ++i) sb.Append("a");
+            tests1w[0] = new TestP(sb.ToString(), 1);
+            tests1w[1] = new TestP(sb.ToString() + "b", 100001);
+            int count;
+            int len;
+            int[] ind;
+            int? cei;
+            Console.WriteLine();
 
-        count=0;
-        Console.WriteLine("Testy szablonu - podstawowe");
-        for ( int i=0 ; i<tests1.Length ; ++i )
+            count = 0;
+            Console.WriteLine("Testy szablonu - podstawowe");
+            for (int i = 0; i < tests1.Length; ++i)
             {
-            Console.Write("Test {0,2}:  ",i);
-            len=MinPattern(tests1[i].text, out ind);
-            if ( Verify(tests1[i].text,tests1[i].len,len,ind) )
+                Console.Write("Test {0,2}:  ", i);
+                len = MinPattern(tests1[i].text, out ind);
+                if (Verify(tests1[i].text, tests1[i].len, len, ind))
                     ++count;
             }
-        Console.WriteLine();
+            Console.WriteLine();
 
-        TestE[] tests2 = {
+            TestE[] tests2 = {
             new TestE("aaaaa","aaaaa",0),
             new TestE("abab","baba",1),
             new TestE("abc","abd",null),
@@ -74,75 +74,75 @@ public static partial class Lab14
             new TestE("bcdefa","abcdef",5),
             };
 
-        count=0;
-        Console.WriteLine("Testy rownowaznosci cyklicznej (KMR)");
-        for ( int i=0 ; i<tests2.Length ; ++i )
+            count = 0;
+            Console.WriteLine("Testy rownowaznosci cyklicznej (KMR)");
+            for (int i = 0; i < tests2.Length; ++i)
             {
-            Console.Write("Test {0,2}:  ",i+tests1.Length);
-            cei=CyclicEquivalenceKMP(tests2[i].text1, tests2[i].text2);
-            if ( cei==tests2[i].ind )
+                Console.Write("Test {0,2}:  ", i + tests1.Length);
+                cei = CyclicEquivalenceKMP(tests2[i].text1, tests2[i].text2);
+                if (cei == tests2[i].ind)
                 {
-                Console.WriteLine("OK");
-                ++count;
+                    Console.WriteLine("OK");
+                    ++count;
                 }
-            else
-                if ( cei==null || tests2[i].ind==null )
+                else
+                    if (cei == null || tests2[i].ind == null)
                     Console.WriteLine("blad");
                 else
                     Console.WriteLine("dobrze ropoznano rownowaznosc, ale zle przesuniecie");
 
             }
-        Console.WriteLine();
+            Console.WriteLine();
 
-        count=0;
-        Console.WriteLine("Testy rownowaznosci cyklicznej (bezposrednio)");
-        for ( int i=0 ; i<tests2.Length ; ++i )
+            count = 0;
+            Console.WriteLine("Testy rownowaznosci cyklicznej (bezposrednio)");
+            for (int i = 0; i < tests2.Length; ++i)
             {
-            Console.Write("Test {0,2}:  ",i+tests1.Length+tests2.Length );
-            cei=CyclicEquivalenceDirect(tests2[i].text1, tests2[i].text2);
-            if ( cei==tests2[i].ind )
+                Console.Write("Test {0,2}:  ", i + tests1.Length + tests2.Length);
+                cei = CyclicEquivalenceDirect(tests2[i].text1, tests2[i].text2);
+                if (cei == tests2[i].ind)
                 {
-                Console.WriteLine("OK");
-                ++count;
+                    Console.WriteLine("OK");
+                    ++count;
                 }
-            else
-                if ( cei==null || tests2[i].ind==null )
+                else
+                    if (cei == null || tests2[i].ind == null)
                     Console.WriteLine("blad");
                 else
                     Console.WriteLine("dobrze ropoznano rownowaznosc, ale zle przesuniecie");
 
             }
-        Console.WriteLine();
+            Console.WriteLine();
 
-        count=0;
-        Console.WriteLine("Testy szablonu - wydajnosc");
-        for ( int i=0 ; i<tests1w.Length ; ++i )
+            count = 0;
+            Console.WriteLine("Testy szablonu - wydajnosc");
+            for (int i = 0; i < tests1w.Length; ++i)
             {
-            Console.Write("Test {0,2}:  ",i+tests1.Length+2*tests2.Length);
-            len=MinPattern(tests1w[i].text, out ind);
-            if ( Verify(tests1w[i].text,tests1w[i].len,len,ind) )
+                Console.Write("Test {0,2}:  ", i + tests1.Length + 2 * tests2.Length);
+                len = MinPattern(tests1w[i].text, out ind);
+                if (Verify(tests1w[i].text, tests1w[i].len, len, ind))
                     ++count;
             }
-        Console.WriteLine();
+            Console.WriteLine();
 
         }
 
-    private static bool Verify(string text, int expected_len, int len, int[] ind)
+        private static bool Verify(string text, int expected_len, int len, int[] ind)
         {
-        if ( len!=expected_len )
+            if (len != expected_len)
             {
-            Console.WriteLine("nieprawidlowa dlugosc szablonu");
-            return false;
+                Console.WriteLine("nieprawidlowa dlugosc szablonu");
+                return false;
             }
-        foreach ( int i in ind )
-            for ( int j=0 ; j<len ; ++j )
-                if ( text[i+j]!=text[j] )
+            foreach (int i in ind)
+                for (int j = 0; j < len; ++j)
+                    if (text[i + j] != text[j])
                     {
-                    Console.WriteLine("szablonu umieszczony od pozycji {0} nie pasuje",i);
-                    return false;
+                        Console.WriteLine("szablonu umieszczony od pozycji {0} nie pasuje", i);
+                        return false;
                     }
-        Console.WriteLine("OK");
-        return true;
+            Console.WriteLine("OK");
+            return true;
         }
 
     }
