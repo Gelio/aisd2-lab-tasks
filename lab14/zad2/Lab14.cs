152f36d1f59730eda5cd816158e10ee5ed33fd39
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-06-02 18:35:28 +0200

    lab14/zad2: complete text template part

--- lab14/zad2/Lab14.cs
+++ lab14/zad2/Lab14.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Linq;
+using System.Runtime.InteropServices;
 using ASD.Text;
 
 namespace ASD2
@@ -16,8 +18,51 @@ namespace ASD2
         /// <returns>Długość szablonu</returns>
         public static int MinPattern(string text, out int[] indexes)
         {
-            indexes = new int[1] { 0 };
-            return text.Length;
+            int[] P = StringMatching.ComputeP(text);
+            int shortestPattern = -1;
+            List<int> shortestPatternMatches = null;
+            int previousPrefixLength = -1;
+            foreach (int prefixLength in P.OrderByDescending(x => x))
+            {
+                if (prefixLength == previousPrefixLength)
+                    continue;
+                previousPrefixLength = prefixLength;
+                if (prefixLength == 0)
+                    break;
+
+                List<int> patternMatches = IsPattern(text, text.Substring(0, prefixLength));
+                if (patternMatches != null)
+                {
+                    shortestPattern = prefixLength;
+                    shortestPatternMatches = patternMatches;
+                }
+            }
+
+            if (shortestPattern == -1)
+            {
+                indexes = new int[0];
+                return 0;
+            }
+
+            indexes = shortestPatternMatches.ToArray();
+            return shortestPattern;
+        }
+
+        private static List<int> IsPattern(string text, string prefix)
+        {
+            int[] prefixP = StringMatching.ComputeP(prefix);
+            List<int> matches = StringMatching.KMP(text, prefix, prefixP);
+
+            for (int i = 1; i < matches.Count; i++)
+            {
+                if (matches[i] - matches[i - 1] > prefix.Length)
+                    return null;
+            }
+
+            if (matches.Last() + prefix.Length < text.Length)
+                return null;
+
+            return matches;
         }
 
         /// <summary>
