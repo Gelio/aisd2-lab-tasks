f7169dbe4b9d398a765830511379bc156628018c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-06-03 18:19:55 +0200

    lab14/zad4: complete first part

--- lab14/zad4/Huffman.cs
+++ lab14/zad4/Huffman.cs
@@ -37,6 +37,29 @@ namespace Teksty
         {
 
             // ETAP 1 - tu należy zaimplementować tworzenie drzewa Huffmana
+            Dictionary<char, long> characterFrequency = new Dictionary<char, long>();
+            foreach (char c in content)
+            {
+                if (characterFrequency.ContainsKey(c))
+                    characterFrequency[c]++;
+                else
+                    characterFrequency[c] = 1;
+            }
+            PriorityQueue<Node> queue = new PriorityQueue<Node>((n1, n2) => n1.freq < n2.freq);
+            foreach (var characterWithFrequency in characterFrequency)
+                queue.Put(new Node(characterWithFrequency.Key, characterWithFrequency.Value));
+
+            while (queue.Count > 1)
+            {
+                Node n1 = queue.Get(),
+                    n2 = queue.Get();
+
+                Node result = new Node(n1.freq + n2.freq, n1, n2);
+                queue.Put(result);
+            }
+
+            root = queue.Get();
+
 
             codesMap = new Dictionary<char, BitList>();
             buildCodesMap(root, new BitList());
