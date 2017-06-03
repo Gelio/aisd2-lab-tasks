9fa82eb5394b342e9a69ab7065b3930648a111e1
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-06-03 18:09:26 +0200

    lab14/zad4: create project, format code

--- lab14/zad4/BitList.cs
+++ lab14/zad4/BitList.cs
@@ -1,96 +1,98 @@
 ﻿
 namespace ASD
 {
-using System.Collections.Generic;
-using System.Text;
+    using System.Collections.Generic;
+    using System.Text;
 
-public class BitList
+    public class BitList
     {
 
-    // UWAGA:  Każdy bajt to 8 bitów !!!
-    private List<byte> bits;
+        // UWAGA:  Każdy bajt to 8 bitów !!!
+        private List<byte> bits;
 
-    public BitList()
+        public BitList()
         {
-        bits = new List<byte>();
-        Count = 0;
-        return;
+            bits = new List<byte>();
+            Count = 0;
+            return;
         }
 
-    public BitList(BitList bl)
+        public BitList(BitList bl)
         {
-        bits = new List<byte>(bl.bits);
-        Count = bl.Count;
-        return;
+            bits = new List<byte>(bl.bits);
+            Count = bl.Count;
+            return;
         }
 
-    public BitList(string str)
+        public BitList(string str)
         {
-        bits = new List<byte>(str.Length<<3);
-        for ( int i=0 ; i<str.Length ; ++i )
-            Append(str[i]!='0');
+            bits = new List<byte>(str.Length << 3);
+            for (int i = 0; i < str.Length; ++i)
+                Append(str[i] != '0');
         }
 
-    // UWAGA:  To liczba zapamiętanych bitów, a nie liczba elementów listy bits (bo każdy element to bajt!)
-    public int Count { get; private set; }
+        // UWAGA:  To liczba zapamiętanych bitów, a nie liczba elementów listy bits (bo każdy element to bajt!)
+        public int Count { get; private set; }
 
-    public bool this[int index]
+        public bool this[int index]
         {
-        get {
-            if ( index<0 || index>=Count ) throw new System.IndexOutOfRangeException();
-            return ( bits[index>>3] & (1<<(index&7)) ) !=0 ;
+            get
+            {
+                if (index < 0 || index >= Count) throw new System.IndexOutOfRangeException();
+                return (bits[index >> 3] & (1 << (index & 7))) != 0;
             }
-        set {
-            if ( index<0 || index>=Count ) throw new System.IndexOutOfRangeException();
-            if ( value )
-                bits[index>>3] |= (byte)(1<<(index&7));
-            else
-                bits[index>>3] &= (byte)~(1<<(index&7));
+            set
+            {
+                if (index < 0 || index >= Count) throw new System.IndexOutOfRangeException();
+                if (value)
+                    bits[index >> 3] |= (byte)(1 << (index & 7));
+                else
+                    bits[index >> 3] &= (byte)~(1 << (index & 7));
             }
         }
 
-    public BitList Append(bool b)
+        public BitList Append(bool b)
         {
-        if ( (Count&0x7)==0 ) bits.Add(0);
-        ++Count;
-        this[Count-1]=b;
-        return this;
+            if ((Count & 0x7) == 0) bits.Add(0);
+            ++Count;
+            this[Count - 1] = b;
+            return this;
         }
 
-    public BitList Append(BitList bl)
+        public BitList Append(BitList bl)
         {
-        for ( int i=0 ; i<bl.Count ; ++i )
-            Append(bl[i]);
-        return this;
+            for (int i = 0; i < bl.Count; ++i)
+                Append(bl[i]);
+            return this;
         }
 
-    public override string ToString()
+        public override string ToString()
         {
-        StringBuilder sb = new StringBuilder();
-        for ( int i=0 ; i<Count ; ++i )
-            sb.Append(this[i]?'1':'0');
-        return sb.ToString();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Count; ++i)
+                sb.Append(this[i] ? '1' : '0');
+            return sb.ToString();
         }
 
-    public override bool Equals(object obj)
+        public override bool Equals(object obj)
         {
-        BitList b = obj as BitList;
-        if ( b==null )
-            return false;
-        if ( bits.Count!=b.bits.Count )
-            return false;
-        for ( int i=0 ; i<bits.Count ; ++i )
-            if ( bits[i]!=b.bits[i] )
+            BitList b = obj as BitList;
+            if (b == null)
                 return false;
-        return true;
+            if (bits.Count != b.bits.Count)
+                return false;
+            for (int i = 0; i < bits.Count; ++i)
+                if (bits[i] != b.bits[i])
+                    return false;
+            return true;
         }
 
-    public override int GetHashCode()
+        public override int GetHashCode()
         {
-        int res=0;
-        for ( int i=0 ; i<bits.Count ; ++i )
-            res ^= ((int)bits[i])<<((i&3)<<3);
-        return res;
+            int res = 0;
+            for (int i = 0; i < bits.Count; ++i)
+                res ^= ((int)bits[i]) << ((i & 3) << 3);
+            return res;
         }
 
     }  // class BitList
--- lab14/zad4/Huffman.cs
+++ lab14/zad4/Huffman.cs
@@ -5,64 +5,64 @@ using ASD;
 namespace Teksty
 {
 
-public partial class Huffman
+    public partial class Huffman
     {
 
-    private class Node
+        private class Node
         {
 
-        public char character;
-        public long freq;
-        public Node left, right;
+            public char character;
+            public long freq;
+            public Node left, right;
 
-        public Node(char character, long freq)
+            public Node(char character, long freq)
             {
-            this.character = character;
-            this.freq = freq;
+                this.character = character;
+                this.freq = freq;
             }
 
-        public Node(long freq, Node left, Node right)
+            public Node(long freq, Node left, Node right)
             {
-            this.freq = freq;
-            this.left = left;
-            this.right = right;
+                this.freq = freq;
+                this.left = left;
+                this.right = right;
             }
 
         }
 
-    private Node root;
-    private Dictionary<char, BitList> codesMap;
+        private Node root;
+        private Dictionary<char, BitList> codesMap;
 
-    public Huffman (string content)
+        public Huffman(string content)
         {
 
-        // ETAP 1 - tu należy zaimplementować tworzenie drzewa Huffmana
+            // ETAP 1 - tu należy zaimplementować tworzenie drzewa Huffmana
 
-        codesMap = new Dictionary<char,BitList>();
-        buildCodesMap(root,new BitList());
+            codesMap = new Dictionary<char, BitList>();
+            buildCodesMap(root, new BitList());
         }
 
-    private void buildCodesMap(Node node, BitList code)
+        private void buildCodesMap(Node node, BitList code)
         {
 
-        // ETAP 2  - tu należy zaimplementować generowanie kodów poszczególnych znaków oraz wypełnianie mapy codesMap
+            // ETAP 2  - tu należy zaimplementować generowanie kodów poszczególnych znaków oraz wypełnianie mapy codesMap
 
         }
 
-    public BitList Compress(string content)
+        public BitList Compress(string content)
         {
 
-        // ETAP 2 - wykorzystując dane w codesMap należy zakodować napis przekazany jako parametr content
+            // ETAP 2 - wykorzystując dane w codesMap należy zakodować napis przekazany jako parametr content
 
-        return null;
+            return null;
         }
 
-    public string Decompress(BitList compressed)
+        public string Decompress(BitList compressed)
         {
 
-        // ETAP 3 - należy zwrócić zdekodowane dane
+            // ETAP 3 - należy zwrócić zdekodowane dane
 
-        return null;
+            return null;
         }
 
     }
